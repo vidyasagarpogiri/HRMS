8a65d5ede1a5f8da79c759b059c6d40ba1c0fe59
Ramarao <ramarao@ramarao.(none)>
2014-08-25 19:52:15 +0530

    pattabhi rempved confllicts

--- db/migrate/20140825114734_change_role_id_employee.rb
+++
@@ -1,8 +0,0 @@
-class ChangeRoleIdEmployee < ActiveRecord::Migration
-  
-  def up
-    rename_column :employees, :Role_id, :role_id
-  end
-  
-
-end
--- db/schema.rb
+++ db/schema.rb
@@ -128,11 +128,7 @@ ActiveRecord::Schema.define(version: 20140825123850) do
   add_index "employees", ["job_location_id"], name: "index_employees_on_job_location_id", using: :btree
   add_index "employees", ["permanent_address_id"], name: "index_employees_on_permanent_address_id", using: :btree
   add_index "employees", ["present_address_id"], name: "index_employees_on_present_address_id", using: :btree
-<<<<<<< HEAD
-  add_index "employees", ["role_id"], name: "index_employees_on_Role_id", using: :btree
-=======
   add_index "employees", ["role_id"], name: "index_employees_on_role_id", using: :btree
->>>>>>> 7ef72a419a85fd6a951490811e52d44008e0c418
 
   create_table "ff_statuses", force: true do |t|
     t.string   "status_name"
