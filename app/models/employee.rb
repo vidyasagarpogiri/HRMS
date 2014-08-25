ca91a689a3cbf1d40ecfac1ea07ab99d9fedc861
Ramarao <ramarao@ramarao.(none)>
2014-08-25 12:26:23 +0530

    pattabhi- added relations and associations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,13 @@
 class Employee < ActiveRecord::Base
   belongs_to :department
+  belongs_to :blood_group
+  belongs_to :designation
+  belongs_to :job_location
+  belongs_to :address
+  belongs_to :education
+  belongs_to :ff_status
+  belongs_to :role
+  belongs_to :grade
+  has_many :promotions
  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822135246) do
+ActiveRecord::Schema.define(version: 20140825065052) do
 
   create_table "addresses", force: true do |t|
     t.text     "line1"
@@ -52,6 +52,11 @@ ActiveRecord::Schema.define(version: 20140822135246) do
 
   create_table "desginations", force: true do |t|
     t.string   "desgination_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "designations", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
