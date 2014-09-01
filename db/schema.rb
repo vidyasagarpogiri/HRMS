8c444be342803fa50548ebfcabb982bb2a737149
Balaraju <vankalabalaraju@gmail.com>
2014-09-01 20:14:17 +0530

    BalaRaju - Added

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20140901123523) do
-=======
-ActiveRecord::Schema.define(version: 20140901113436) do
->>>>>>> 351444ccd5c58056e735c6b780140248ceeee8aa
 
   create_table "addresses", force: true do |t|
     t.text     "line1"
