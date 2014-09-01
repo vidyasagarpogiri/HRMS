8d2aec3393931e1733e3935e46c5ab76467722d6
Vidyasagar <vidyasgarpogiri@gmail.com>
2014-09-01 16:00:17 +0530

    sekhar - changes made to migrations

--- db/migrate/20140901094012_add_columns_to_address.rb
+++ db/migrate/20140901094012_add_columns_to_address.rb
@@ -4,8 +4,6 @@ class AddColumnsToAddress < ActiveRecord::Migration
 		add_column :addresses, :state, :string
 		add_column :addresses, :country, :string
 		add_column :addresses, :zipcode, :string
-		remove_column :addresses, :city_id, :integer
-		remove_column :addresses, :state_id, :integer
-		remove_column :addresses, :country_id, :integer
+		
   end
 end
