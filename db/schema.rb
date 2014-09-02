91ed824a191381b57eb8a2bdb6e8183251531193
Priyanka <priyankamuddana.it@gmail.com>
2014-09-02 12:49:39 +0530

    priyanka-worked with salary views

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20140901133604) do
-=======
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20140901123523) do
-=======
-ActiveRecord::Schema.define(version: 20140901113436) do
->>>>>>> 351444ccd5c58056e735c6b780140248ceeee8aa
->>>>>>> 9bc255e0e2e00384748b88c932867c07837cb9fe
 
   create_table "addresses", force: true do |t|
     t.text     "line1"
