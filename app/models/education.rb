d9e6564c58e6eeac09359d4c32ec73d220b525a7
Ramarao <ramarao@ramarao.(none)>
2014-09-03 14:51:11 +0530

    vidyasagar - added validations and * mark to emp new

--- app/models/education.rb
+++ app/models/education.rb
@@ -5,7 +5,10 @@ class Education < ActiveRecord::Base
   has_many :education_qualifications
   
   
- 
+  validates :specilization, presence: true
+	validates :institute, presence: true
+	validates :year_of_pass, presence: true
+  validates :cgpa_percentage, presence: true
 	
 
 end
