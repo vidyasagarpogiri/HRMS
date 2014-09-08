b29d6437947593b9961f0a0c6c00890b436e4542
Priyanka <priyankamuddana.it@gmail.com>
2014-09-08 19:13:37 +0530

    leave managment

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,5 +4,8 @@ class Group < ActiveRecord::Base
   has_many :holiday_calenders
   has_one :reporting_manager
   
+  def reporting_manager_full_name
+   reporting_manager.employee.full_name if reporting_manager.present?
+  end
   
 end
