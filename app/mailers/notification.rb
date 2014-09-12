160f158e9da73d05c18631d15550632dc6114b44
Ramarao <ramarao@ramarao.(none)>
2014-09-12 17:13:16 +0530

    vidyasagar resolved conflicts

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -13,13 +13,13 @@ class Notification < ActionMailer::Base
 	def accept_leave(employee, leave_history)
 		@employee = employee
 		@leave_history = leave_history
-		 mail(:to => @employee.user.email, :subject => "#{@employee.first_name} for leave #{@leave_history.leave_type.type_name} for #{@leave_history.days} day(s) ")
+		 mail(:to => @employee.user.email, :subject => "#{@leave_history.leave_type.type_name} leave has been approved for #{@leave_history.days} day(s) ")
 	end
   
 	def reject_leave(employee, leave_history)
 		@employee = employee
 		@leave_history = leave_history
-		 mail(:to => @employee.user.email, :subject => "#{@employee.first_name} for leave #{@leave_history.leave_type.type_name} for #{@leave_history.days} day(s) ")
+		 mail(:to => @employee.user.email, :subject => "#{@leave_history.leave_type.type_name} leave has been rejected for #{@leave_history.days} day(s) ")
 	end
 end
 
