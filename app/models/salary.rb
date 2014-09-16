ef3b5b830d6561c7742ad7fae099215319e93cc3
Balaraju <vankalabalaraju@gmail.com>
2014-09-16 16:04:42 +0530

    BalaRaju - added salary

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -7,4 +7,10 @@ class Salary < ActiveRecord::Base
   validates :ctc_fixed, presence: true
 	validates :basic_salary, presence: true
   
+  
+  after_save :ctc_sum
+  
+  def ctc_sum
+    self.ctc_fixed = 
+  end
 end
