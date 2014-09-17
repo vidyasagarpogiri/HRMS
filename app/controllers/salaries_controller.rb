b60a486a64529ecb914ce307bbe74ce423faea99
Priyanka <priyankamuddana.it@gmail.com>
2014-09-17 18:18:21 +0530

    profile view

--- app/controllers/salaries_controller.rb
+++ app/controllers/salaries_controller.rb
@@ -1,6 +1,6 @@
 class SalariesController < ApplicationController
 	include ApplicationHelper
-   layout "emp_profile_template", only: [:index, :new, :create, :show, :edit, :update]
+   layout "emp_profile_template", only: [:index, :new, :create, :show, :edit, :update, :configure_allowance]
 
 		before_filter :user_authentication, only: [:index, :new, :create, :show, :edit, :update]
 
