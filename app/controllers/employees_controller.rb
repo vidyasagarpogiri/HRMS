8b3cc16cf2b26e972975c4ea25c7b9109d88c6b3
Ramarao <ramarao@ramarao.(none)>
2014-09-19 11:50:56 +0530

    pattabhi modified levave histories index view

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -56,12 +56,15 @@ class EmployeesController < ApplicationController
       params["employee_attachments"]["attachment"].each_with_index do |a, i|
       #raise a.inspect
         @employee_attachment = @employee.employee_attachments.create!(:attachment => a, :attachment_name => params["employee_attachments"]["attachment_name"][i], :employee_id => @employee.id)
+        redirect_to attachment_show_employee_path(@employee)  
       end
-      render 'attachment_form_new'
+     
          #redirect_to attachment_show_employee_path(@employee)
     end
-    redirect_to attachment_show_employee_path(@employee)  
-   end
+    else
+     render 'attachment_form_new'
+     
+    end
     if params[:employee].present? 
       if @employee.update(params_employees) 
         @report = @employee.reporting_managers.first
