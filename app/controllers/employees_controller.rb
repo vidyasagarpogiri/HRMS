7dff9f237f1d46cc2c9f3824258dcb8abaf6b282
Balaraju <vankalabalaraju@gmail.com>
2014-09-23 11:04:53 +0530

    BalaRaju - added code for employee managemnet

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -58,11 +58,12 @@ class EmployeesController < ApplicationController
       params["employee_attachments"]["attachment"].each_with_index do |a, i|
       #raise a.inspect
         @employee_attachment = @employee.employee_attachments.create!(:attachment => a, :attachment_name => params["employee_attachments"]["attachment_name"][i], :employee_id => @employee.id)
-        redirect_to attachment_show_employee_path(@employee)  
+        
       end
      
-         #redirect_to attachment_show_employee_path(@employee)
+        
     end
+       redirect_to attachment_show_employee_path(@employee)
     end
     
     if params[:employee].present? 
