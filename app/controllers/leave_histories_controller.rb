bcbaf6e48c633468fc7d227a7701d814915477ba
Ramarao <ramarao@ramarao.(none)>
2014-09-30 20:33:34 +0530

    added ajax pagination to leave histories

--- app/controllers/leave_histories_controller.rb
+++ app/controllers/leave_histories_controller.rb
@@ -7,8 +7,8 @@ class LeaveHistoriesController < ApplicationController
 
 	def index
 		@leave = current_user.employee.department.leave_policy
-		@leaves = current_user.employee.leave_histories.where(:status => 'HOLD').page(params[:page]).per(4)
-		@leave_histories = current_user.employee.leave_histories.where("status != ?", "HOLD" ).page(params[:page]).per(4)
+		@leaves = current_user.employee.leave_histories.where(:status => 'HOLD').page(params[:page]).per(2)
+		@leave_histories = current_user.employee.leave_histories.where("status != ?", "HOLD" ).page(params[:page]).per(2)
 		@employee = current_user.employee
 		@holiday_calenders = current_user.employee.department.holiday_calenders
 	end
