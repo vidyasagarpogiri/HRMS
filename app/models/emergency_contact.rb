6e4baf32e1578e535c558ba873b513ea262e9c35
Vidyasagar <vidyasgarpogiri@gmail.com>
2014-10-07 11:48:59 +0530

    modified error msg in emergency model

--- app/models/emergency_contact.rb
+++ app/models/emergency_contact.rb
@@ -1,8 +1,8 @@
 class EmergencyContact < ActiveRecord::Base
   belongs_to :employee
   
-  validates :name, presence: true, format: { with: /\A[a-zA-Z\s ]+\z/, message: "Plese Enter only Alphabets" }
-  validates :relation, presence: true, format: { with: /\A[a-zA-Z\s ]+\z/, message: "Plese Enter only Alphabets" }
+  validates :name, presence: true, format: { with: /\A[a-zA-Z\s ]+\z/, message: "Please Enter only Alphabets" }
+  validates :relation, presence: true, format: { with: /\A[a-zA-Z\s ]+\z/, message: "Please Enter only Alphabets" }
   validates :mobile1, presence: true, numericality: true , length: { maximum: 10, minimum:10 }
   validates :mobile2, numericality: true , length: { maximum: 10, minimum:10 }
 end
