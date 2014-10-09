cca1a96ea066e946246ecbdd9c747e2cf233f832
sekharberi <sekhar.mca270@gmail.com>
2014-10-09 11:40:20 +0530

    added pagination

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,10 +5,18 @@ class WelcomeController < ApplicationController
   end
   
   def dashboard
+  #raise params.inspect
+    if params[:page1].present?
+      @pages = "Event"
+    elsif params[:page2].present?
+      @pages = "announcments"
+    elsif params[:page3].present?
+      @pages = "jobopenings"
+    end
    unless current_user.department == Department::HR
-    @welcome_event = AmzurEvent.all.page(params[:page]).per(2)
-    @welcome_announcements = Announcement.all.page(params[:page]).per(2)
-    @welcome_recruitments = Recruitment.where(:status => "open").page(params[:page]).per(2)
+    @welcome_event = AmzurEvent.all.page(params[:page1]).per(2)
+    @welcome_announcements = Announcement.all.page(params[:page2]).per(2)
+    @welcome_recruitments = Recruitment.where(:status => "open").page(params[:page3]).per(2)
     @employee = current_user.employee
    else
     redirect_to employees_path
