013ac95f19b655b067e4dc64a01e41311984d3e2
Vidyasagar <vidyasgarpogiri@gmail.com>
2014-10-17 10:27:05 +0530

    sagar - added emailer to announcements migration file to employee and allowances

--- db/migrate/20141016111425_add_is_deductable_to_allowance.rb
+++ db/migrate/20141016111425_add_is_deductable_to_allowance.rb
@@ -1,5 +1,5 @@
 class AddIsDeductableToAllowance < ActiveRecord::Migration
   def change
-    add_column :allowances, :is_deductable, :boolean
+    add_column :allowances, :is_deductable, :boolean, :default => false
   end
 end
