96fc1414943c72386bf1c753d09119789ea34507
Ramarao <ramarao@ramarao.(none)>
2014-10-20 16:06:46 +0530

    pattabhi added job locations edit new index views

--- app/controllers/job_locations_controller.rb
+++ app/controllers/job_locations_controller.rb
@@ -9,7 +9,28 @@ class JobLocationsController < ApplicationController
  end
  
  def create
- raise params.inspect
+  @address = Address.new
+  @address=Address.create(:line => params[:line],:line1=> params[:line1],:city=> params[:city],:state=> params[:state],:country=> params[:country],:zipcode=> params[:zipcode])
+  @job_location= JobLocation.create(:address_id => @address.id)
+  redirect_to job_locations_path
  end
  
+ def edit
+ 
+  @job_location = JobLocation.find(params[:id])
+  @address = @job_location.address
+  #raise @job_location.inspect
+ end
+ 
+ def update
+  @job_location = JobLocation.find(params[:id])
+  @address = @job_location.address.update(:line => params[:line],:line1=> params[:line1],:city=> params[:city],:state=> params[:state],:country=> params[:country],:zipcode=> params[:zipcode])
+   redirect_to job_locations_path
+ end
 end
+
+
+
+
+
+ 
