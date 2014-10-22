6cb493120a3cda957d2b442eb401bca7c2246590
Vidyasagar <vidyasgarpogiri@gmail.com>
2014-10-22 16:06:52 +0530

    sagar - resolved conflicts in migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007130929) do
+ActiveRecord::Schema.define(version: 20141020092633) do
 
   create_table "addresses", force: true do |t|
     t.text     "line1"
