3ace034c2c2e5668869c922e5a1bd6b3bf19b93b
Ramarao <ramarao@ramarao.(none)>
2014-10-22 16:14:28 +0530

    conflicts resolved

--- db/migrate/20141020092633_add_bank_details_to_employee.rb
+++
@@ -1,7 +0,0 @@
-class AddBankDetailsToEmployee < ActiveRecord::Migration
-  def change
-    add_column :employees, :bank_name, :string
-    add_column :employees, :branch_name, :string
-    add_column :employees, :account_number, :string
-  end
-end
