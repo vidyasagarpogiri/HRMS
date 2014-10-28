1a572f9b546f376a7eb9088560f8bd7541713b57
Priyanka <priyankamuddana.it@gmail.com>
2014-10-28 18:51:45 +0530

    Priyanka promotions UI issues resolved

--- db/schema.rb
+++ db/schema.rb
@@ -326,6 +326,8 @@ ActiveRecord::Schema.define(version: 20141028101505) do
     t.float    "pt",                 limit: 24
     t.float    "tds",                limit: 24
     t.float    "special_allowance",  limit: 24
+    t.integer  "month"
+    t.integer  "year"
   end
 
   create_table "payslips_allowances", force: true do |t|
