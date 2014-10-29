1f4c665fb55d116f98b3c86bbd745e72e0462a25
Ramarao <ramarao@ramarao.(none)>
2014-10-29 17:50:14 +0530

    pattabhi - modified investment declarations edit views

--- app/controllers/investment_declarations_controller.rb
+++ app/controllers/investment_declarations_controller.rb
@@ -8,7 +8,7 @@ class InvestmentDeclarationsController < ApplicationController
     i_ids = InvestmentDeclaration.all.map(&:general_investment_id)
     remaining_ids = g_ids-i_ids
     remaining_ids.each do |id|
-      InvestmentDeclaration.create(general_investment_id: id, monthly_value: 0, yearly_value: 0, employee_id: current_user.employee.id )
+      InvestmentDeclaration.create(general_investment_id: id, yearly_value: 0, employee_id: current_user.employee.id )
     end
     @declarations = InvestmentDeclaration.all
   end
@@ -20,7 +20,7 @@ class InvestmentDeclarationsController < ApplicationController
   def update
     @declaration = InvestmentDeclaration.find(params[:id])
     #raise params[:investment_declaration][:monthly_value].inspect
-     @declaration.update(:general_investment_id =>  @declaration.general_investment_id, :monthly_value => params[:investment_declaration][:monthly_value], :yearly_value => params[:investment_declaration][:yearly_value], :employee_id => current_user.employee.id)
+     @declaration.update(:general_investment_id =>  @declaration.general_investment_id, :yearly_value => params[:investment_declaration][:yearly_value], :employee_id => current_user.employee.id)
      redirect_to investment_declarations_path
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 20141029105600) do
-
 
   create_table "addresses", force: true do |t|
     t.text     "line1"
@@ -364,12 +361,7 @@ ActiveRecord::Schema.define(version: 20141029105600) do
     t.float    "pt",                 limit: 24
     t.float    "tds",                limit: 24
     t.float    "special_allowance",  limit: 24
-<<<<<<< HEAD
-=======
-    t.integer  "month"
-    t.integer  "year"
     t.string   "status"
->>>>>>> f73a26fe1a2a136b361f45e382ba269a8201bea0
   end
 
   create_table "payslips_allowances", force: true do |t|
