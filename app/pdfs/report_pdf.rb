33b71ba5fc137a1af628dcf9fc9d139786cc60a0
Balaraju <vankalabalaraju@gmail.com>
2014-11-01 18:01:10 +0530

    BalaRaju - Working on Payslip pdf module

--- app/pdfs/report_pdf.rb
+++ app/pdfs/report_pdf.rb
@@ -3,21 +3,84 @@ class ReportPdf < Prawn::Document
     super()
     @payslip = payslip
     header
-    text_content
-    table_content
+    employee_details
+    employee_salary
+    total_salary
+    #text_content
+    #table_content
   end
  
   def header
     #This inserts an image in the pdf file and sets the size of the image
-    image "#{Rails.root}/app/assets/images/amzur-logo.png", :position => :left, :width => 50, :height => 30
+    image "#{Rails.root}/app/assets/images/amzur-logo.png", :position => :right, :width => 150, :height => 80
+    draw_text "PaySlip", style: :bold,  size: 15, :at => [230, 700]
   end
  
+  def employee_details
+    draw_text "MONTH/YEAR:", :at => [50, 600] #1
+    draw_text "#{@payslip.month}-#{@payslip.year}", :at => [150, 600]
+    draw_text "ATTD:", :at => [300, 600]
+    draw_text "MONTH/YEAR", :at => [400, 600]
+    draw_text "EMP CODE:", :at => [50, 580] #2
+    draw_text "MONTH/YEAR2:", :at => [150, 580]
+    draw_text "DEPARTMENT:", :at => [300, 580]
+    draw_text "MONTH/YEAR4:", :at => [400, 580]
+    draw_text "NAME:", :at => [50, 560] #3
+    draw_text "MONTH/YEAR2:", :at => [150, 560]
+    draw_text "PF No:", :at => [300, 560]
+    draw_text "MONTH/YEAR4:", :at => [400, 560]
+    draw_text "LOCATION:", :at => [50, 540] #4
+    draw_text "MONTH/YEAR2:", :at => [150, 540]
+    draw_text "PAN No:", :at => [300, 540]
+    draw_text "MONTH/YEAR4:", :at => [400, 540]
+    draw_text "MODE:", :at => [50, 520] #5
+    draw_text "MONTH/YEAR2:", :at => [150, 520]
+    draw_text "A/c No:", :at => [300, 520]
+    draw_text "MONTH/YEAR4:", :at => [400, 520]
+    stroke_color "000000"
+    fill_color "666222"
+    stroke do
+      fill_and_stroke_rectangle [10, 480], 520, 20 
+    end
+    fill_color "000000"
+    font("Times-Roman") do
+      draw_text "EARNINGS:", :at => [50, 465]
+      draw_text "DEDUCTIONS", :at => [300, 465]
+    end
+  end
+  
+  def employee_salary
+    draw_text "BASIC:", :at => [50, 440] #1
+    draw_text "#{@payslip.month}-#{@payslip.year}", :at => [150, 440]
+    draw_text "TDS:", :at => [300, 440]
+    draw_text "MONTH/YEAR", :at => [400, 440]
+    draw_text "HRA:", :at => [50, 420] #2
+    draw_text "#{@payslip.month}-#{@payslip.year}", :at => [150, 420]
+    draw_text "PT:", :at => [300, 420]
+    draw_text "MONTH/YEAR", :at => [400, 420]
+  end
+  
+  def total_salary
+    stroke_color "000000"
+    fill_color "666222"
+    stroke do
+      fill_and_stroke_rectangle [10, 400], 520, 20 
+    end
+    fill_color "000000"
+    font("Times-Roman") do
+      draw_text "TOTAL:", :at => [50, 385]
+      draw_text "100000", :at => [150, 385]
+      draw_text "TOTAL:", :at => [300, 385]
+      draw_text "1000", :at => [400, 385]
+    end
+  end
+  
   def text_content
     stroke_axis
     stroke_color "ff0000"
     stroke do
      # just lower the current y position
-       move_down 50
+       move_down 500
        horizontal_rule
        vertical_line 100, 300, :at => 50
        horizontal_line 200, 500, :at => 150
@@ -25,9 +88,7 @@ class ReportPdf < Prawn::Document
 
    
  
-    
-      text "Duis vel", size: 15, style: :bold
-      text "Duis vel tortor elementum, ultrices tortor vel, accumsan dui. Nullam in dolor rutrum, gravida turpis eu, vestibulum lectus. Pellentesque aliquet dignissim justo ut fringilla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut venenatis massa non eros venenatis aliquet. Suspendisse potenti. Mauris sed tincidunt mauris, et vulputate risus. Aliquam eget nibh at erat dignissim aliquam non et risus. Fusce mattis neque id diam pulvinar, fermentum luctus enim porttitor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+   
   
   end
  
