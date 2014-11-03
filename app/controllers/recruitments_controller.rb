f4f9fa53d6f679fe2f37eb1978d34fae0871e2f3
Vidyasagar <vidyasgarpogiri@gmail.com>
2014-11-03 18:49:43 +0530

    sagar - added hot or regular job field in recruitment

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -17,7 +17,7 @@ class RecruitmentsController < ApplicationController
       #raise  params.inspect
       #Notification.job_notification(@users,@recruitment).deliver
       Employee.where(status: false).each do |emp|
-      Notification.delay.job_notification(emp.user,@recruitment)
+      #Notification.delay.job_notification(emp.user,@recruitment)
       end 	
        #raise Notification.delay.job_notification(@users,@recruitment).inspect
       redirect_to recruitments_path
@@ -57,7 +57,7 @@ class RecruitmentsController < ApplicationController
   private
   
   def recruitment_params
-     params.require(:recruitment).permit(:jobcode, :title, :description, :link, :status, :file ) 
+     params.require(:recruitment).permit(:jobcode, :title, :description, :link, :status, :file, :job_type) 
   end
   
 end
