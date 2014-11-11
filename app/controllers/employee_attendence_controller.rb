ec3780862b7eec676d05507b50f24e4a2b828312
Priyanka <priyankamuddana.it@gmail.com>
2014-11-11 14:41:27 +0530

    Employee Attandance View changed

--- app/controllers/employee_attendence_controller.rb
+++ app/controllers/employee_attendence_controller.rb
@@ -54,10 +54,13 @@ class EmployeeAttendenceController < ApplicationController
   end
   
   def show_attendance
+    @employees =  Employee.where(:status => false)
+=begin  
    if current_user.department == Department::HR
     @employees =  Employee.where(:status => false)
    elsif current_user.degnation 
-   end  
+   end
+=end     
   end
 
   def new_attendence_log
