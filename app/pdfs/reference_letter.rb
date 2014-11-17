6df3a4365ddfd0ec75cd230fbe9d1187071e6986
Balaraju <vankalabalaraju@gmail.com>
2014-11-17 15:51:33 +0530

    BalaRaju - Working on Dynamic PDF

--- app/pdfs/reference_letter.rb
+++ app/pdfs/reference_letter.rb
@@ -1,7 +1,8 @@
 class ReferenceLetter < Prawn::Document
-  def initialize(user)
+  def initialize(user, hr_manager)
     super()
     @user = user
+    @hr_manager = hr_manager
     @y = 700
     @x = 0
     logo
@@ -17,7 +18,7 @@ class ReferenceLetter < Prawn::Document
   end
   
   def date
-    draw_text "#{Time.now.strftime("%Y-%m-%d")}", size: 12, :at => [@x+40, @y-100]
+    draw_text "#{Time.now.strftime("%d %b, %Y")}", size: 12, :at => [@x+40, @y-100]
   end
   
   def header
@@ -27,13 +28,13 @@ class ReferenceLetter < Prawn::Document
   def text
     draw_text "This is to certify that  #{@user.employee.title}.#{@user.employee.full_name} is a bonafide employee of our Company" , size: 12, :at => [@x+40, @y-200]
     draw_text "(Amzur Technologies Pvt Ltd) and is working as #{@user.employee.designation.designation_name if @user.employee.designation.present?}. We confirm that" , size: 12, :at => [@x+40, @y-220] 
-    draw_text "he/she has been working in our company since #{@user.employee.date_of_join}.", size: 12, :at => [@x+40, @y-240]
+    draw_text "he/she has been working in our company since #{@user.employee.date_of_join.to_date.strftime("%d %b, %Y") if @user.employee.date_of_join.present? }.", size: 12, :at => [@x+40, @y-240]
 
   end
   
   def sign
     draw_text "Yours sincerely,", :at => [@x+40, @y-340] 
-    draw_text "<HR Name>", :at => [@x+40, @y-400]
+    draw_text "#{@hr_manager},", :at => [@x+40, @y-400]
     draw_text "Human Resources", :at => [@x+40, @y-420]
   end 
  
