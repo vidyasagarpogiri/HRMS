9eeaa6c3b0d4ba7993579c087f6a1205c92f70e9
Vidyasagar <vidyasgarpogiri@gmail.com>
2014-11-19 12:42:34 +0530

    sagar - modified mailers

--- app/controllers/amzur_events_controller.rb
+++ app/controllers/amzur_events_controller.rb
@@ -16,7 +16,7 @@ class AmzurEventsController < ApplicationController
    @users = User.all
    if @amzurevent.save
     Employee.where(status: false).each do |emp|
-    Notification.delay.event_notification(emp.user,@amzurevent)
+    Notification.event_notification(emp.user,@amzurevent).deliver
     end
     redirect_to amzur_events_path
    else
