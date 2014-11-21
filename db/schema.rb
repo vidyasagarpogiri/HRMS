52b4d0298a3d507dd1eac532402a5fbbc47f850d
Ramarao <ramarao@ramarao.(none)>
2014-11-21 15:55:46 +0530

    pattabhi - conflicts resolved

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20141212095632) do
+ActiveRecord::Schema.define(version: 20141212095631) do
 
 
   create_table "addresses", force: true do |t|
