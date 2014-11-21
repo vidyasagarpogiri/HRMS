fb6831933aa5f1d4b550c416130e445fad699cb2
Ramarao <ramarao@ramarao.(none)>
2014-11-21 16:44:53 +0530

    pattabhi created projects model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20141121102056) do
-
-
+ActiveRecord::Schema.define(version: 20141121103447) do
 
   create_table "addresses", force: true do |t|
     t.text     "line1"
