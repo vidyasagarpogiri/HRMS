b0fd6952ec7494bc4ced45b03775ea8682a2e4e6
Priyanka <priyankamuddana.it@gmail.com>
2014-11-21 16:07:26 +0530

    Priyanka photo and album deleted

--- spec/factories/albums.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :album do
-    title "MyString"
-description "MyText"
-employee_id 1
-  end
-
-end
--- spec/factories/photos.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :photo do
-    album "MyString"
-album_id 1
-  end
-
-end
