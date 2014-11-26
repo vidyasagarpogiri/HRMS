5ada5d826a3d199d25f02f9c4a8246457975782c
Priyanka <priyankamuddana.it@gmail.com>
2014-11-26 15:13:38 +0530

    Priyanka checked album and photo album in spec with reek and rubocop

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Album, :type => :model do
-  describe "Checking Album Model Level Validattions" do
-  
-    it "Create Albums With Valid Deatils" do
+RSpec.describe Album, type: 'model' do
+  describe 'Checking Album Model Level Validattions' do
+
+    it 'Create Albums With Valid Deatils' do
       expect(FactoryGirl.create(:album)).to be_valid
     end
-    
-    it "Is invalid without attributes" do
+
+    it 'Is invalid without attributes' do
       expect(FactoryGirl.build(:album, title: nil, description: nil, employee_id: nil)).to_not be_valid
     end
-    
-    it "Is invalid without title" do
-      expect(FactoryGirl.build(:album, title: nil, description: "qqq", employee_id: 1)).to_not be_valid
+
+    it 'Is invalid without title' do
+      expect(FactoryGirl.build(:album, title: nil, description: 'qqq', employee_id: 1)).to_not be_valid
     end
-    
-    it "Is invalid without Description" do
-      expect(FactoryGirl.build(:album, title: "Album", description: nil, employee_id: 1)).to_not be_valid
+
+    it 'Is invalid without Description' do
+      expect(FactoryGirl.build(:album, title: 'Album', description: nil, employee_id: 1)).to_not be_valid
     end
-    
-    it "Is invalid without id" do
-      expect(FactoryGirl.build(:album, title: "Album", description: "qqq", employee_id: nil)).to_not be_valid
+
+    it 'Is invalid without id' do
+      expect(FactoryGirl.build(:album, title: 'Album', description: 'qqq', employee_id: nil)).to_not be_valid
     end
   end
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Photo, :type => :model do
-  describe "checking Photo Model level Validation" do
-    it "creating photo valid with valid details" do
+RSpec.describe Photo, type: 'model' do
+  describe 'checking Photo Model level Validation' do
+    it 'creating photo valid with valid details' do
       expect(FactoryGirl.create(:photo)).to be_valid
     end
-    
-    it "invalid witout album_id" do
-      expect(FactoryGirl.build(:photo, album_id: nil, image: "qqq")).to_not be_valid
+
+    it 'invalid witout album_id' do
+      expect(FactoryGirl.build(:photo, album_id: nil, image: 'qqq')).to_not be_valid
     end
-    
-    it "invalid witout image" do
+
+    it 'invalid witout image' do
       expect(FactoryGirl.build(:photo, album_id: 1, image: nil)).to_not be_valid
     end
-    
-    it "invalid witout image and album_id" do
+
+    it 'invalid witout image and album_id' do
       expect(FactoryGirl.build(:photo, album_id: nil, image: nil)).to_not be_valid
     end
   end
