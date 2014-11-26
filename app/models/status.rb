f28785b9a7596901b969f0762d3aa2dc2a8f2c9c
Vidyasagar <vidyasgarpogiri@gmail.com>
2014-11-26 13:55:34 +0530

    sagar - added rspec files to status

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,4 +2,6 @@ class Status < ActiveRecord::Base
   belongs_to :employee  # status belongs to employee
   has_many :comments  # status may have more than one comment so has_many relaionship
   has_many :likes #one status may have more than one like
+  validates :status, presence: true #validation for status
+  validates :employee_id, presence: true #validation for employee
 end
--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -8,16 +8,33 @@ RSpec.describe Status, :type => :model do
       expect(FactoryGirl.create(:status)).to be_valid
     end
     
-    it "Is invalid without status" do
-      expect(FactoryGirl.build(:status, status: nil)).to_not be_valid
+    it "Is invalid without status " do
+      expect(FactoryGirl.build(:status, status: nil, employee_id: 1, likes_count: nil)).to_not be_valid
     end
+    
+    it "Is invalid without employee" do
+      expect(FactoryGirl.build(:status, status: "test", employee_id: nil, likes_count: 5)).to_not be_valid
+    end
+    
     
     it "Is valid without likes count" do
       expect(FactoryGirl.build(:status, status: "hello this is status", likes_count: nil)).to be_valid
     end
     
-     it "Is invalid without status" do
-      expect(FactoryGirl.build(:status, status: nil, likes_count: '10')).to_not be_valid
+     it "Is valid with status and likescount" do
+      expect(FactoryGirl.build(:status, status: "hiii", likes_count: '0')).to be_valid
+    end
+    
+    it "Is invalid with status and without emp and likescount" do
+      expect(FactoryGirl.build(:status, status: "hiii", employee_id: nil, likes_count: nil)).to_not be_valid
+    end
+    
+    it "Is invalid without status, emp and likescount" do
+      expect(FactoryGirl.build(:status, status: nil , employee_id: nil, likes_count: nil)).to_not be_valid
+    end
+    
+     it "Is invalid without any field in status" do
+      expect(FactoryGirl.build(:status, status: nil , employee_id: nil, likes_count: nil)).to_not be_valid
     end
     
    end
