c3cbf6458cac0347d3ccfce8384695f39cabde26
Priyanka <priyankamuddana.it@gmail.com>
2014-11-26 14:18:45 +0530

    Priyanka Adding testcases for album

--- spec/factories/albums.rb
+++ spec/factories/albums.rb
@@ -1,8 +1,32 @@
 FactoryGirl.define do
   factory :album do
-    title "MyString"
-description "MyText"
-employee_id 1
+    title "Myalbum"
+    description "B'day party pics"
+    employee_id 1
   end
 
+  factory :invalid_album , parent: :album do
+    title nil
+    description nil
+    employee_id nil
+  end
+  
+  factory :invalid_title_album , parent: :album do
+    title nil
+    description "qqq"
+    employee_id 1
+  end
+  
+  factory :invalid_description_album , parent: :album do
+    title "Album"
+    description nil
+    employee_id 1
+  end
+  
+  factory :invalid_id_album , parent: :album do
+    title "Album"
+    description "qqq"
+    employee_id nil
+  end
+  
 end
