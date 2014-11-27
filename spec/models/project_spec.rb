afddb74e5ab2af28357272808265d6673aaf9cf4
Ramarao <ramarao@ramarao.(none)>
2014-11-27 19:29:10 +0530

    pattabhi modified test cases in rspec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -7,15 +7,15 @@ RSpec.describe Project, :type => :model do
       end
       
       it " is not valid without title" do
-      expect(Project.create(title:nil, description: "some text", start_date: "Fri, 21 Sep 2014", end_date: "Fri, 21 Nov 2014", roles: "some role1,some role2", tasks_performed: "task1,task2", skills: "skill1,skill2", employee_id: 1)).to be_valid
+      expect(Project.create(title:nil, description: "some text", start_date: "Fri, 21 Sep 2014", end_date: "Fri, 21 Nov 2014", roles: "some role1,some role2", tasks_performed: "task1,task2", skills: "skill1,skill2", employee_id: 1)).to_not be_valid
       end
       
       it " is not valid without employee_id" do
-      expect(Project.create(title: "HRMS", description: "some text", start_date: "Fri, 21 Sep 2014", end_date: "Fri, 21 Nov 2014", roles: "some role1,some role2", tasks_performed: "task1,task2", skills: "skill1,skill2", employee_id: nil)).to be_valid
+      expect(Project.create(title: "HRMS", description: "some text", start_date: "Fri, 21 Sep 2014", end_date: "Fri, 21 Nov 2014", roles: "some role1,some role2", tasks_performed: "task1,task2", skills: "skill1,skill2", employee_id: nil)).to_not be_valid
       end
       
       it " is not valid without title and employee_id" do
-      expect(Project.create(title: nil, description: "some text", start_date: "Fri, 21 Sep 2014", end_date: "Fri, 21 Nov 2014", roles: "some role1,some role2", tasks_performed: "task1,task2", skills: "skill1,skill2", employee_id: nil)).to be_valid
+      expect(Project.create(title: nil, description: "some text", start_date: "Fri, 21 Sep 2014", end_date: "Fri, 21 Nov 2014", roles: "some role1,some role2", tasks_performed: "task1,task2", skills: "skill1,skill2", employee_id: nil)).to_not be_valid
       end
   end
 end
