09fa67424d4053898f1bd1ed76da27dcd911dc0a
Balaraju <vankalabalaraju@gmail.com>
2014-11-27 17:41:52 +0530

    BalaRaju - modifed some code in the status and likes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,14 @@
 # this controller is for status comments
 class CommentsController < ApplicationController
   def index
-    # raise params.inspect
-    # @employee = current_user.employee
-    # @status = Status.find(params[:status_id])
     @comments = Comment.order('created_at ASC').limit(3)
-    # @like = Like.all
-    # raise @comments.inspect
   end
 
   def create
-    # raise params.inspect
     @employee = current_user.employee
     @status = Status.find(params[:status_id])
     @comment = @status.comments.create(comment_params)
     if @comment.save
-      # Notification.comment_notification(@employee, @comment, @status).deliver
-      # raise Notification.inspect
       redirect_to statuses_path
     else
       render 'new'
@@ -29,8 +21,7 @@ class CommentsController < ApplicationController
     @comment = Comment.new
   end
 
-  def destroy
-  end
+
 
   private
 
