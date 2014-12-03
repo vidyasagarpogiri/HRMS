b78007ac450c65abdd1af84923052cc23125adb0
Balaraju <vankalabalaraju@gmail.com>
2014-12-03 18:21:46 +0530

    BalaRaju - Added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -137,3 +137,14 @@ TaxBracket.create(bracket: "High Level", lower_limit: 500001, upper_limit: 10000
 TaxBracket.create(bracket: "Very High Level", lower_limit: 1000001, upper_limit: 12500000000, tax_percentage: 30, min_tax: 125000) 
 
 
+SectionDeclaration.create(section: "80C", maximum_limit: 25000)
+SectionDeclaration.create(section: "80D", maximum_limit: 25000)
+
+GeneralInvestment.create(title: "Life Insurance Premiums", description: "Need Documents", section_declaration_id: 1) 
+GeneralInvestment.create(title: "Home Loan Principal Repayment", description: "Need Documents", section_declaration_id: 1) 
+GeneralInvestment.create(title: "Provident Fund", description: "Need Documents", section_declaration_id: 1) 
+GeneralInvestment.create(title: "Medical Insurance", description: "Need Documents", section_declaration_id: 2) 
+
+
+
+
