2ab4bb575d9d8e89e762cff7f582c0fad6df0d27
Balaraju <vankalabalaraju@gmail.com>
2014-12-11 10:29:41 +0530

    BalaRaju - Added Organization

--- app/models/reporting_manager.rb
+++ app/models/reporting_manager.rb
@@ -7,4 +7,29 @@ class ReportingManager < ActiveRecord::Base
      "#{self.employee.full_name}"
   end
   
+  
+  def self.reporting_data
+    a = []
+    Employee.all.each do |emp|
+      a << {id: emp.id, employees: report_emp(emp.id) }
+    end
+    a
+  end
+  
+  def manager
+    Employee.find(manager_id)
+  end
+  
+  def manager_employees
+    employees = ReportingManager.where(manager_id: manager_id).map(&:employee)
+  end 
+  
+  def reportee_employees
+    employees = ReportingManager.where(manager_id: employee_id).map(&:employee).map(&:id)
+  end
+  
+  def self.report_emp(emp)
+    employees = ReportingManager.where(manager_id: emp).map(&:employee).map(&:id)
+  end
+  
 end
