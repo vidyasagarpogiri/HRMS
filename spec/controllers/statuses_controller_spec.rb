12fb010ab14dc8ada05b5db1319918814bbf4618
Vidyasagar <vidyasgarpogiri@gmail.com>
2014-12-11 12:30:04 +0530

    sagar - added statuses rspec

--- spec/controllers/statuses_controller_spec.rb
+++ spec/controllers/statuses_controller_spec.rb
@@ -178,28 +178,42 @@ RSpec.describe StatusesController, :type => :controller do
          post :create, {status:  FactoryGirl.attributes_for(:status)}
          expect(response).to redirect_to(statuses_path)
       end
-      it "post like" do
+      it "Add like to status" do
       expect{
        post :add_like, {status_id: status.id, like: FactoryGirl.attributes_for(:like, is_like: true, employee_id: 1, status_id: status.id)}
        }
         end
-        it "post unlike" do
+        it "Add unlike to status" do
       expect{
        post :remove_like, {status_id: status.id, like: FactoryGirl.attributes_for(:like, is_like: false, employee_id: 1, status_id: status.id)}
        }
         end
       end
-    end   
+    end
+
+    describe "POST # create un-like" do 
+    context "Add like  status  with valid attributes" do
+      it "creates a new status" do
+        expect {
+          post :create, {status:  FactoryGirl.attributes_for(:status)}
+        }.to change(Status, :count).by(1)
+      end 
+      
+      it "redirects to the status create" do
+         post :create, {status:  FactoryGirl.attributes_for(:status)}
+         expect(response).to redirect_to(statuses_path)
+      end
+      it "post like to status" do
+      expect{
+       post :add_like, {status_id: status.id, like: FactoryGirl.attributes_for(:like, is_like: true, employee_id: 1, status_id: status.id)}
+       }
+        end
+        it "post unlike to status" do
+      expect{
+       post :remove_like, {status_id: status.id, like: FactoryGirl.attributes_for(:like, is_like: false, employee_id: 1, status_id: status.id)}
+       }
+        end
+      end
+    end      
       
 end
-
-
-
-
-
-
-
-
-
-
-
