cba1b1c18a70588fa9baaf41fc2c1cc00ab968bb
Balaraju <vankalabalaraju@gmail.com>
2014-12-24 14:38:26 +0530

    BalaRaju _ added schdular

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -84,8 +84,10 @@ end
       leave = employee.leave || employee.create_leave(pl_carry_forward_prev_year: 0, available_leaves: 0)
 	    a_leaves = leave.available_leaves ||  0
 	    cf_leaves = leave.pl_carry_forward_prev_year || 0
-	    leave_policy = employee.group.leave_policy
-      if leave_policy.present?
+	    
+	    group = employee.group
+      if group.present? && group.leave_policy.present?
+        leave_policy = group.leave_policy
         a_leaves += leave_policy.pl_this_year
         leave.update(available_leaves: a_leaves)
       end
