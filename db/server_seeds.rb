5276b36417377e8138c26013577635ff7ffcff94
Balaraju <vankalabalaraju@gmail.com>
2015-01-06 12:24:43 +0530

    Balaraju - Added code

--- db/server_seeds.rb
+++ db/server_seeds.rb
@@ -14,54 +14,82 @@
 
 Address.destroy_all
 JobLocation.destroy_all
+
 BloodGroup.destroy_all
 FfStatus.destroy_all
+
+
 Employee.destroy_all
+#Education.destroy_all
+#Experience.destroy_all
+#Promotion.destroy_all
 Salary.destroy_all
 LeaveType.destroy_all
 Group.destroy_all
 Allowance.destroy_all
 Event.destroy_all 
 StaticSalary.destroy_all
+Skill.destroy_all
 
 
+ @user = User.invite!(:email =>  "hr-admin@amzur.com", :skip_invitation => true)
+ Address.create(:line1 => "9-29-22,2nd Floor,Pioneer Sankar Shantiniketan", :line => "Balaji Nagar,Siripuram", :city => "Visakhapatnam", :state=>"Andhra Pradesh",:country=>"India",:zipcode=>"530003")
+addresses = Address.all.pluck(:id)
 
-#sekharberi@1989
-@user = User.invite!(:email =>  "balaraju.vankala@amzur.com", :skip_invitation => true)
- 
 
+(addresses).each do 
+  JobLocation.create( :address_id => addresses[rand(addresses.length)])
+end
+JobLocations = JobLocation.all.pluck(:id)
 
-["O+ve", "O-ve", "A+ve", "A+-e", "B+ve", "B-ve", "AB+ve", "AB-ve"].each do |bloodgrp|
+# We are already created deparment and designations in Migration file
+Departments = Department.all.pluck(:id)
+
+Grades = Grade.all.pluck(:id)
+#-----------------------------------------------------------------------------------------------------#
+
+["O+ve", "O-ve", "A+ve", "A-ve", "B+ve", "B-ve", "AB+ve", "AB-ve"].each do |bloodgrp|
   BloodGroup.create(:blood_group_name => bloodgrp ) 
 end
 BloodGroups = BloodGroup.all.pluck(:id)
 
-address = Address.create(:line1 => "street 1",:line => "near KFC",:city => "Vizag",:state=>"Andhra",:country=>"India",:zipcode=>"530003")
-JobLocation.create( :address_id => address.id )
-
-
-Employee.create(:employee_id => 1, :title=> "Mr", :first_name=>"Sekhar",:last_name=>"Beri",:date_of_birth=>"2014-12-30", :gender=>"male",:mobile_number=>"9876543219",:father_name=>"Adinarayana",:date_of_join=>"2014-03-03",:image_url=> "image",:department_id =>2,:blood_group_id=> BloodGroups[rand(BloodGroups.length)],:avatar=>"406929_2668579486846_928068538_n.jpg", :user_id => @user.id, :alternate_email => "balaraju.vankala@gmail.com")
-
-Employees = Employee.all.pluck(:id)
+["Open", "Hold", "Closed"].each do |ffstatus|
+  FfStatus.create(:status_name => ffstatus ) 
+end
+FfStatuses = FfStatus.all.pluck(:id)
 
 
 
-["Personal Leave", "Carry Forward Leave"].each do |leave|
+["Hr Group", "Development", "Accounts"].each do |group|
+  Group.create(:group_name => group)
+end
+Groups = Group.all.pluck(:id)
+
+Department.find_by_department_name("HR").designations.create(designation_name: "HR ADMIN") 
+dept_id = Department.find_by_department_name("HR").id
+desg_id = Designation.find_by_designation_name("HR ADMIN").id
+group_id = Group.find_by_group_name("Hr Group").id
+
+Employee.create( :employee_id => 1, :title=>"Mr", :first_name=>"HR", :last_name=>"Admin", :date_of_birth=>"30/12/2001", :gender=>"male", :marital_status=>"single", :total_experience=>"6.5",
+:status=>"Active", :mobile_number=>"9876543219",:father_name=>"Father",:pan=>"pan", :date_of_join=>"30/12/2001", :image_url=> "image",
+:department_id => dept_id, :blood_group_id=> BloodGroups[rand(BloodGroups.length)],
+:ff_status_id => FfStatuses[rand(FfStatuses.length)], 
+:job_location_id => JobLocations[rand(JobLocations.length)], :avatar => "406929_2668579486846_928068538_n.jpg", :user_id => @user.id, 
+:alternate_email => "vankalabalaraju@gmail.com", :group_id => group_id, :designation_id => desg_id )
+
+
+
+
+
+
+
+["Personal Leave", "Floating Holiday"].each do |leave|
 LeaveType.create(:type_name => leave ) 
 end
 LeaveTypes = LeaveType.all.pluck(:id)
 
 
-Event.create(:event_name =>"Pongal", :event_date => "14/01/2014")
-Event.create(:event_name =>"Ugadi", :event_date => "11/04/2014")
-Event.create(:event_name=>"Independence Day", :event_date => "15/08/2014")
-Event.create(:event_name =>"Raksha Bhandan", :event_date => "23/08/2014")
-Event.create(:event_name =>"Vinayaka Chaturthi", :event_date => "29/08/2014")
-Event.create(:event_name =>"Dussehra", :event_date => "04/10/2014")
-Event.create(:event_name =>"Diwali", :event_date => "14/11/2014")
-Event.create(:event_name =>"Naga Panchami", :event_date => "21/11/2014")
-Event.create(:event_name =>"Thanks Giving Day", :event_date => "27/11/2014")
-Event.create(:event_name =>"Christmas", :event_date => "25/12/2014")
+
 
 StaticSalary.create(:name => "Basic", :value => 40.0)
 StaticSalary.create(:name => "PF", :value => 12)
@@ -69,6 +97,7 @@ StaticSalary.create(:name => "PF Contribution", :value => 13.61)
 StaticSalary.create(:name => "Esic", :value => 1.75)
 StaticSalary.create(:name => "Esic Contribution", :value => 4.25)
 StaticSalary.create(:name => "HRA", :value => 25)
+
 
 #Tax Brackets
 
@@ -78,11 +107,12 @@ TaxBracket.create(bracket: "High Level", lower_limit: 500001, upper_limit: 10000
 TaxBracket.create(bracket: "Very High Level", lower_limit: 1000001, upper_limit: 12500000000, tax_percentage: 30, min_tax: 125000) 
 
 
-SectionDeclaration.create(section: "80C", maximum_limit: 25000)
-SectionDeclaration.create(section: "80D", maximum_limit: 25000)
 
-GeneralInvestment.create(title: "Life Insurance Premiums", description: "Need Documents", section_declaration_id: 1) 
-GeneralInvestment.create(title: "Home Loan Principal Repayment", description: "Need Documents", section_declaration_id: 1) 
-GeneralInvestment.create(title: "Provident Fund", description: "Need Documents", section_declaration_id: 1) 
-GeneralInvestment.create(title: "Medical Insurance", description: "Need Documents", section_declaration_id: 2) 
+
+['c','c++','ruby', 'rails','java','ios','android','asp.net','jquery','ajax','css','javascript','mysql','sqlite','oracle','pl/sql'].each do |skill|
+  Skill.create(:name => skill ) 
+end
+
+
+
 
