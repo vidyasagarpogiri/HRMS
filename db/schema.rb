2ebf7062a7efae21f182fe16312430fd379b12f0
Balaraju <vankalabalaraju@gmail.com>
2015-01-14 15:50:57 +0530

    Balaraju - Added Audit Module

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114053451) do
+ActiveRecord::Schema.define(version: 20150114100246) do
 
   create_table "addresses", force: true do |t|
     t.text     "line1"
@@ -108,6 +108,29 @@ ActiveRecord::Schema.define(version: 20150114053451) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "audits", force: true do |t|
+    t.integer  "auditable_id"
+    t.string   "auditable_type"
+    t.integer  "associated_id"
+    t.string   "associated_type"
+    t.integer  "user_id"
+    t.string   "user_type"
+    t.string   "username"
+    t.string   "action"
+    t.text     "audited_changes"
+    t.integer  "version",         default: 0
+    t.string   "comment"
+    t.string   "remote_address"
+    t.string   "request_uuid"
+    t.datetime "created_at"
+  end
+
+  add_index "audits", ["associated_id", "associated_type"], name: "associated_index", using: :btree
+  add_index "audits", ["auditable_id", "auditable_type"], name: "auditable_index", using: :btree
+  add_index "audits", ["created_at"], name: "index_audits_on_created_at", using: :btree
+  add_index "audits", ["request_uuid"], name: "index_audits_on_request_uuid", using: :btree
+  add_index "audits", ["user_id", "user_type"], name: "user_index", using: :btree
 
   create_table "blood_groups", force: true do |t|
     t.string   "blood_group_name"
