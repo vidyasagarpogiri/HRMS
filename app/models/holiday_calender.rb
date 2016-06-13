2d4bf9607d7a8044ee60933c93cb6f807464c943
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-06-13 13:37:51 +0530

    Added validations
    Added validations

--- app/models/holiday_calender.rb
+++ app/models/holiday_calender.rb
@@ -2,7 +2,5 @@ class HolidayCalender < ActiveRecord::Base
   belongs_to :department
   belongs_to :group
   belongs_to :event
-  
   validates :event_id, :uniqueness => {:scope => :department_id}
-  
 end
