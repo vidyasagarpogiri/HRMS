641ded26928aa06656758f711d558ce42baa3acc
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-06-15 13:10:12 +0530

    Updated Routes
    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'getAllEmployees' => 'employees#getAllEmployees'  
 
   #devise_for :users
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
