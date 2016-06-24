3db84e4f87786eba15daee3dbb8a84306ab2a2db
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-06-24 13:30:51 +0530

    Added validation
    Added validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -16,7 +16,7 @@ class Employee < ActiveRecord::Base
   has_many :promotions
   has_many :educations
   has_many :experiences
-	has_many :email_ettiquities, :class_name => "EmailEttiquitie"
+  has_many :email_ettiquities, :class_name => "EmailEttiquitie"
 
 
 
