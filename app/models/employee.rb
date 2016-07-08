1ce3d00a047f98c5b993045ccafec339e0e9cd62
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-07-08 18:36:42 +0530

    Added associations
    Added associations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -42,8 +42,6 @@ class Employee < ActiveRecord::Base
 	validates :mobile_number, presence: true, numericality: true , length: { maximum: 10 }
 
 
-
-
 	validates :father_name, presence: true
 	validates :blood_group_id, presence: true
 	validates :date_of_join, presence: true
