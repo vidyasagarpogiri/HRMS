493173e1c45b60699e04251f68b50262af8221de
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-07-28 14:00:59 +0530

    Done refactoring for new method
    Done refactoring for new method

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -14,9 +14,9 @@ class AddressesController < ApplicationController
   def new
     @employee = Employee.find(params[:employee_id])
     @address = Address.new
-  	@address_type_value, @address_type = false, "Present" if params[:address_type]=="false" 
- 	@address_type_value, @address_type = true, "Permanent" if params[:address_type]=="true"
- 	@copy_address_type = @address_type == "Present" ? "Permanent":"Present" if @address_type.present?
+    @address_type_value, @address_type = false, "Present" if params[:address_type]=="false" 
+    @address_type_value, @address_type = true, "Permanent" if params[:address_type]=="true"
+    @copy_address_type = @address_type == "Present" ? "Permanent":"Present" if @address_type.present?
   end
   
   def create
