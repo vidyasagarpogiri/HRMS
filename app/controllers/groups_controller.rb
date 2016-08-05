1eefdf49c90d75196161164d7bad51225406ec2f
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-08-05 16:06:17 +0530

    Done refactoring
    Done refactoring

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,22 +3,20 @@ class GroupsController < ApplicationController
     layout "leave_template"
    before_filter :hr_view,  only: ["new", "edit"]
   before_filter :other_emp_view
+  
   def index
     @groups = Group.all
   end
   
   def new
-    
     @group = Group.new
     @employee = Employee.new
     @reporting_manager = ReportingManager.new
      
   end
-  
  
   
   def create
-    
     @employee = Employee.find(params[:emp_id]) 
     @group = Group.create(group_params)
     @reporting_manager = ReportingManager.create(:employee_id => @employee.id, :group_id => @group.id)
@@ -30,25 +28,21 @@ class GroupsController < ApplicationController
      @employee = @group.reporting_manager.employee
      @leave_policy = @group.leave_policy
      @holiday_calenders = @group.holiday_calenders
-     
   end
   
   def edit
      @group = Group.find(params[:id])
      @reporting_manager = ReportingManager.find_by(@group.id)
      @employee = Employee.find_by(@reporting_manager.id)
-     
   end
   
   def update
-   
     @group = Group.find(params[:id])
     @group.update(group_params)
     @employee = Employee.find(params[:emp_id])
     @reporting_manager = ReportingManager.find_by(@group.id)
     @reporting_manager.update(:employee_id => @employee.id)
     redirect_to  @group
-     
   end
   
   def add_employee
@@ -57,7 +51,6 @@ class GroupsController < ApplicationController
   end
   
   def update_add_employee
-      
       @group = Group.find(params[:id])
       @employee = Employee.find(params[:employee_id])      
       @employee.update(:group_id => @group.id)
