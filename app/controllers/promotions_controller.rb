4f5d69dc6b0e3bf585c24eb2ce154fc390d5ccce
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-08-12 13:04:03 +0530

    Added index method
    Added index method

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -20,11 +20,11 @@ class PromotionsController < ApplicationController
 		@promotion = Promotion.create(:date_of_promotion=> params[:promotion][:date_of_promotion], :employee_id=>params[:employee_id],
 		:designation_id=>params[:promotion][:designation_id])                      
     if @promotion.save
-		@employee.update(:designation_id => @promotion.designation_id)
-		redirect_to employee_promotions_path
-		else 
-		render 'new'
-		end
+	@employee.update(:designation_id => @promotion.designation_id)
+	redirect_to employee_promotions_path
+	else 
+	render 'new'
+	end
   end
   
   def edit
