9bbfd182f9c5c4ac32a497539f5ed8f22c7f3c66
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-08-17 12:09:37 +0530

    Done refactoring for index
    Done refactoring for index

--- app/controllers/holiday_calenders_controller.rb
+++ app/controllers/holiday_calenders_controller.rb
@@ -25,11 +25,9 @@ def index
       if params[:mandatory].include?(event)
       #raise params[:mandatory].inspect
         @holiday_calender = HolidayCalender.create(department_id: @department.id, event_id: event, mandatory_or_optional: true)
- 
       else
         @holiday_calender = HolidayCalender.create(department_id: @department.id, event_id: event, mandatory_or_optional: false)
       end 
-     
     end
        redirect_to leaves_department_path(@department)  
     end
