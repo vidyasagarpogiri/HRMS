287874f6a981ec4a1e6a37991cc717b5fd5e3ec5
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-08-19 11:55:00 +0530

    Added index method
    Added index method

--- app/controllers/insentives_controller.rb
+++ app/controllers/insentives_controller.rb
@@ -1,9 +1,9 @@
 class InsentivesController < ApplicationController
   before_filter :hr_view,  only: ["new", "edit"]
   before_filter :other_emp_view 
-def index
-  #raise params.inspect
   
+ def index
+  #raise params.inspect
   @salary = Salary.find(params[:salary_id])
   @insentive = @salary.insentives
  end
