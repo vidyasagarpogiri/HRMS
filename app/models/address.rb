94c1ff4f6a25f8727e74954b13ddac170a8cf2e2
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-08-30 12:32:05 +0530

    Added validations and associations
    Added validations and associations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ActiveRecord::Base
+	
   belongs_to :employee # belongs to relation
   has_one :job_location # has one realtion
-  
   
   validates :line1, presence: true
   validates :line, presence: true
