6b243d26f3c4569679d03db5b7be79f87f0f971a
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-10-10 21:59:33 +0530

    Done Validations & Associations
    Done Validations & Associations

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,11 +1,9 @@
 class Education < ActiveRecord::Base
 	
   belongs_to :employee
-  belongs_to :city
-	
+  belongs_to :city	
   has_many :qualifications, through: :education_qualifications
   has_many :education_qualifications
-	
   validates :specilization, presence: true
   validates :institute, presence: true
   validates :year_of_pass, presence: true
