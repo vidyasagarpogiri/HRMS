39207f0ac161db4f88fa3df23d10be3e8e08f276
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-10-19 12:58:05 +0530

    Added Validations
    Added Validations

--- app/models/education.rb
+++ app/models/education.rb
@@ -2,6 +2,7 @@ class Education < ActiveRecord::Base
 	
   belongs_to :employee
   belongs_to :city	
+	
   has_many :qualifications, through: :education_qualifications
   has_many :education_qualifications
 	
