7dfefd6613630827e84def84e0a6a337063445d9
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-10-25 12:56:58 +0530

    Added validations
    Added validations

--- app/models/employee_attachment.rb
+++ app/models/employee_attachment.rb
@@ -3,7 +3,6 @@ class EmployeeAttachment < ActiveRecord::Base
   mount_uploader :attachment, AttachmentUploader
   
   belongs_to :employee
-  
   #Attachment Validations
   #validates :attachment, presence: true
   #validates :attachment_name, presence: true
