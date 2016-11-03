4f37f815f171b275c3ebd7b04f85cfa8542509d0
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-11-03 13:30:47 +0530

    Done refactoring
    Done refactoring

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -17,15 +17,10 @@ class EducationsController < ApplicationController
   end
   
   def create
-  
-
-               
+     
     @new_education = Education.create(params.require(:education).permit(:specilization, :institute, :year_of_admission, :year_of_pass, :cgpa_percentage).merge(Employee_id: params[:employee_id]))
     @errors = @new_education.errors.full_messages
-    #raise @errors.inspect
-
-  
-    
+    #raise @errors.inspect     
     #for new form 
     @employee = Employee.find(params[:employee_id])
     @education = Education.new
