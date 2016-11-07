44b0c25ff00ce6641fc6498d38f51b51548f57f0
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-11-07 14:05:13 +0530

    Added Validations
    Added Validations

--- app/models/leave_policy.rb
+++ app/models/leave_policy.rb
@@ -1,8 +1,9 @@
 class LeavePolicy < ActiveRecord::Base
   belongs_to :group
-  
+  belongs_to :department
+	
   validates :pl_this_year, presence: true 
   validates :eligible_carry_forward_leaves, presence: true
-  belongs_to :department
+  
 	
 end
