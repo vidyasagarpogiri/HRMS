5c0fb8be3bcbf52d2f8d2b2a6161c7035cecff79
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-11-08 13:10:12 +0530

    Added associations
    Added associations

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,7 +1,9 @@
 class Promotion < ActiveRecord::Base
+  
   belongs_to :employee
   belongs_to :designation
   
   validates :date_of_promotion, presence: true
   validates :designation_id, presence: true
+  
 end
