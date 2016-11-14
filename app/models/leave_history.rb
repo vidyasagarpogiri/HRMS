9de6bfba37d193059775ee08148bc26d46103e17
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-11-14 15:05:56 +0530

    Upated the validations
    Upated the validations

--- app/models/leave_history.rb
+++ app/models/leave_history.rb
@@ -9,7 +9,8 @@ class LeaveHistory < ActiveRecord::Base
   REJECTED = "REJECTED"
   STARTED = "STARTED"
   COMPLETE = "COMPLETE"
-  
+ 
+	
  #validates :from_date, presence: true
  #validates :to_date, presence: true
  #validates :leave_type_id, presence: true
