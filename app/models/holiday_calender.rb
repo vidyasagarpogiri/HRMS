e3f3904a9e85636a8ce3fd69f3f351bb649376d8
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-11-16 12:20:48 +0530

    Updated the associations
    Updated the associations

--- app/models/holiday_calender.rb
+++ app/models/holiday_calender.rb
@@ -3,6 +3,7 @@ class HolidayCalender < ActiveRecord::Base
   belongs_to :department
   belongs_to :group
   belongs_to :event
+  
   validates :event_id, :uniqueness => {:scope => :department_id} 
   
 end
