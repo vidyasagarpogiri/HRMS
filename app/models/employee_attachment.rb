f113d55d85db01769f7769aca3ba8b16ea379b5a
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-11-17 15:44:34 +0530

    Updated associations
    Updated associations

--- app/models/employee_attachment.rb
+++ app/models/employee_attachment.rb
@@ -3,6 +3,7 @@ class EmployeeAttachment < ActiveRecord::Base
   mount_uploader :attachment, AttachmentUploader
   
   belongs_to :employee
+	
   #Attachment Validations
   #validates :attachment, presence: true
   #validates :attachment_name, presence: true
