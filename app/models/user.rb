cc11afa7299bd8097d1a689cf20b4a173f6a2d5b
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-11-18 13:21:41 +0530

    Done refactoring
    Done refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :invitable, :omniauth_providers => [:google_oauth2]
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable, :omniauthable, :invitable, :omniauth_providers => [:google_oauth2]
       
          
   def self.from_omniauth(auth)
