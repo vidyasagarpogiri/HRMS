89b867d8e40426193cf1bacc57fc4b09b4ec8583
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-11-23 11:37:49 +0530

    Added associations
    Added associations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,13 +1,11 @@
 class Address < ActiveRecord::Base
 	
   belongs_to :employee 
-  has_one :job_location  
-
+  has_one :job_location
   validates :line1, presence: true
   validates :line, presence: true
   validates :city, presence: true
   validates :state, presence: true
-	
   validates :country, presence: true
   validates :zipcode, presence: true	
 	
