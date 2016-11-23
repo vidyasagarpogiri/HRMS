fd153425abb31a0f9c2b556a40e161f4cde27933
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-11-23 11:37:31 +0530

    Added associations
    Added associations

--- app/models/designation.rb
+++ app/models/designation.rb
@@ -1,9 +1,7 @@
 class Designation < ActiveRecord::Base
-  
   has_many :employees
   has_many :promotions
   has_many :grades
   belongs_to :departmnet
   validates :designation_name, presence: true, uniqueness: { case_sensitive: false }
-  
 end
