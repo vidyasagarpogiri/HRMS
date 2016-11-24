a633f4c3ecc247fbbe4170e5ea05f8b9e76876f2
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-11-24 12:35:32 +0530

    Added before_filter
    Added before_filter

--- app/controllers/salary_increments_controller.rb
+++ app/controllers/salary_increments_controller.rb
@@ -1,7 +1,8 @@
 class SalaryIncrementsController < ApplicationController
   
-   before_filter :hr_view,  only: ["new", "edit"]
+  before_filter :hr_view,  only: ["new", "edit"]
   before_filter :other_emp_view
+  
   def index
   @salary = Salary.find(params[:salary_id])
   @salary_increment = @salary.salary_increments
