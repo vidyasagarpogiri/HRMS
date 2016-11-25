5cf136882cedc86f40b8f1a952211d3e7a4d280a
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-11-25 12:30:19 +0530

    Added before_filter
    Added before_filter

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,7 +1,8 @@
 class GradesController < ApplicationController
 
-   before_filter :hr_view,  only: ["new", "edit"]
+  before_filter :hr_view,  only: ["new", "edit"]
   before_filter :other_emp_view
+	
   def index
     @grades = Grade.all
   end
