8bc25acedd75400e61b9ca71389ab73735426574
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-12-16 12:45:46 +0530

    Updated Associations
    Updated Associations

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,7 +2,7 @@ class Address < ActiveRecord::Base
 	
   belongs_to :employee 
   has_one :job_location
-	
+  # validations	
   validates :line1, presence: true
   validates :line, presence: true
   validates :city, presence: true
