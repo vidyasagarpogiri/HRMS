3a8a3e63f0b13400b2d32d2e18f25750574ce9dc
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-12-22 14:09:04 +0530

    Added Associations
    Added Associations

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,12 +1,9 @@
 class Experience < ActiveRecord::Base
 	
-belongs_to :employee
-
-  
+belongs_to :employee 
 validates :previous_company, presence: true
 validates :last_designation, presence: true
 validates :from_date, presence: true
 validates :to_date, presence: true
-  
   
 end
