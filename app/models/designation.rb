30e3531c03c8a870ffe33535c2a94bab2c62d7a5
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-12-29 14:51:20 +0530

    Added validations
    Added validations

--- app/models/designation.rb
+++ app/models/designation.rb
@@ -3,6 +3,7 @@ class Designation < ActiveRecord::Base
   has_many :employees
   has_many :promotions
   has_many :grades
+  
   belongs_to :departmnet
   validates :designation_name, presence: true, uniqueness: { case_sensitive: false }
   
