bc3b9bee21b525a5107ab9275797d45292993bc6
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-12-30 13:06:48 +0530

    Added Private Method
    Added Private Method

--- app/controllers/holiday_calenders_controller.rb
+++ app/controllers/holiday_calenders_controller.rb
@@ -8,11 +8,11 @@ def index
    @holiday_calenders =@department.holiday_calenders
  end
     
-    def new
-      @holiday_calender = HolidayCalender.new
-      @department = Department.find(params[:department_id])
-      @events = Event.all
-    end
+  def new
+    @holiday_calender = HolidayCalender.new
+    @department = Department.find(params[:department_id])
+    @events = Event.all
+  end
     
     def create
       @department = Department.find(params[:department_id])
