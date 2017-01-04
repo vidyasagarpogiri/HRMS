bcc15b43f508b3213c1b1fb97439490d340c6f95
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-01-04 13:31:18 +0530

    Added Associations
    Added Associations

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,12 +1,11 @@
 class Department < ActiveRecord::Base
-  
+ 
   has_many :employees
   has_many :reporting_managers
   has_many :designations
   has_many :grades
   has_many :holiday_calenders
   has_many :events, :through => :holiday_calenders
-  
   #TODO since there is a problem in relation with employ and department i have commented this -  vidyasagar
   #has_many :employees, :through => :reporting_managers
   
