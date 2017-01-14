a9c776b4fc379a8a3cb21b95fd05c3239ba1a9ad
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-01-14 20:19:17 +0530

    Added association
    Added association

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ActiveRecord::Base
   
   has_many :holiday_calenders
   has_many :departments, :through => :holiday_calenders
+  
   validates :event_name, uniqueness: true
   validates :event_date, uniqueness: true
   
