3d0654fc314c6d22f5f1d6b2044d19656017daad
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-01-17 13:21:03 +0530

    Updated Associations
    Updated Associations

--- app/models/designation.rb
+++ app/models/designation.rb
@@ -3,8 +3,8 @@ class Designation < ActiveRecord::Base
   has_many :employees
   has_many :promotions
   has_many :grades
-  
   belongs_to :departmnet
+  
   validates :designation_name, presence: true, uniqueness: { case_sensitive: false }
   
 end
