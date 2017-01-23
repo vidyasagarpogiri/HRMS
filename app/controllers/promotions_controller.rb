31f7a9c13410678d3afa91ff484c40476a4b9c9c
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-01-23 14:00:29 +0530

    Modified Private Method
    Modified Private Method

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -15,9 +15,9 @@ class PromotionsController < ApplicationController
   end
     
  def create
- #raise params.inspect
- @employee = Employee.find(params[:employee_id])    
- @promotion = Promotion.create(:date_of_promotion=> params[:promotion][:date_of_promotion], :employee_id=>params[:employee_id],
+   #raise params.inspect
+   @employee = Employee.find(params[:employee_id])    
+   @promotion = Promotion.create(:date_of_promotion=> params[:promotion][:date_of_promotion], :employee_id=>params[:employee_id],
 :designation_id=>param[:promotion][:designation_id])                      
 if @promotion.saves
 @employee.update(:designation_id => @promotion.designation_id)
