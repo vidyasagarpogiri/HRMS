a59047be2b60b8204e13d3164ab66c5c93e5a553
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-01-27 13:42:20 +0530

    Updated  private Method
    Updated  private Method

--- app/controllers/email_ettiquities_controller.rb
+++ app/controllers/email_ettiquities_controller.rb
@@ -7,8 +7,8 @@ class EmailEttiquitiesController < ApplicationController
 	
 def index
 # raise params.inspect
-@employee = Employee.find(params[:employee_id])
-@emails = @employee.email_ettiquities
+ @employee = Employee.find(params[:employee_id])
+ @emails = @employee.email_ettiquities
 end
 
  def new
