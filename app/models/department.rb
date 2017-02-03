58e84f697a240588b8a8c784f9ef1f0cf0958450
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-02-03 14:26:04 +0530

    Added association
    Added association

--- app/models/department.rb
+++ app/models/department.rb
@@ -7,7 +7,7 @@ class Department < ActiveRecord::Base
   has_many :holiday_calenders
   has_many :events, :through => :holiday_calenders
  
-  #TODO since there is a problem in relation with employ and department i have commented this -  vidyasagar
+  # TODO since there is a problem in relation with employ and department i have commented this -  vidyasagar
   #has_many :employees, :through => :reporting_managers
   
   validates :department_name, uniqueness: { case_sensitive: false }, presence: true
