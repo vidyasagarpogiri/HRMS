ca23d3707f4cbbca105a42a92d5d3bd6e1326af4
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-03-13 13:33:18 +0530

    Updated Private Method
    Updated Private Method

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -12,11 +12,10 @@ class GradesController < ApplicationController
   end
 
   def create
-    
     @designation = Designation.find(params[:grade][:designation_id])
     @grade = @designation.grades.create(grade_params)
     redirect_to @designation 
-  end
+  end   
          
   def show
     @grade = Grade.find(params[:id])
