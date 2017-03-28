eb73745dd5f233673f9cd772489480b282c60867
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-03-28 20:50:36 +0200

    Updated Validations
    Updated Validations

--- app/models/department.rb
+++ app/models/department.rb
@@ -6,7 +6,7 @@ class Department < ActiveRecord::Base
   has_many :grades       
   has_many :holiday_calenders
   has_many :events, :through => :holiday_calenders
-                                              
+                                                  
   # TODO since there is a problem in relation with employ and department i have commented this -  vidyasagar
   # has_many : employees, :through => :reporting_managers   
                            
