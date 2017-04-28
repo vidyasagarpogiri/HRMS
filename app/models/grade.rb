ac1ae021fe9b0aeb044e6edfbf9b1758fa72d954
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-04-28 15:16:55 +0530

    Added belongs_to
    Added belongs_to

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,6 +1,6 @@
 class Grade < ActiveRecord::Base
        
-  has_many :employees        
+  has_many :employees                  
   belongs_to :designation    
   # validates :value, uniqueness: { case_sensitive: false }
    
