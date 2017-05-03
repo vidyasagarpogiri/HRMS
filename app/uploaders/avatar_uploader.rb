7d7581ea7814028a8ac107ed5a91e14a8b62d787
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-05-03 17:06:31 +0530

    store_dir
    store_dir

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -22,7 +22,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #                                   
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  # end             
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
