931f984bf51fc621cd5da7f587bf3ff6ead9e5c5
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-05-05 14:09:17 +0530

    validations added
    validations added

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,4 +1,4 @@
-class Promotion < ActiveRecord::Base
+class Promotion < ActiveRecord::Base             
   belongs_to :employee                                         
   belongs_to :designation                        
   validates :date_of_promotion, presence: true
