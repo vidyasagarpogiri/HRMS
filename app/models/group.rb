d75a6006531e172d87c75922beeef3855ef10e51
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-06-07 17:34:10 +0530

    Updated Model
    Updated Model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ActiveRecord::Base
                  
   has_many :employees                                                                                
-  has_one :leave_policy                                                                                
+  has_one :leave_policy                                                                                                                                    
   has_many :holiday_calenders                            
   has_many :events, :through => :holiday_calenders
   has_one :reporting_manager
