021eb3169c6c604b0b86e6edeb4372a15deca9ec
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-06-07 18:09:28 +0530

    Updated validations
    Updated validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
                  
-  has_many :employees                                                                                            
+  has_many :employees                                                                                                          
   has_one :leave_policy                                                                                                                                    
   has_many :holiday_calenders                                         
   has_many :events, :through => :holiday_calenders
