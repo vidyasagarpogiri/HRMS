29cd874e15c86adc8d704b563b13b2eb1c82ca23
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-06-13 14:33:27 +0530

    Updated Models
    Updated Models

--- app/models/designation.rb
+++ app/models/designation.rb
@@ -3,7 +3,7 @@ class Designation < ActiveRecord::Base
   belongs_to :departmnet                 
   has_many :employees           
   has_many :promotions           
-  has_many :grades                                                          
+  has_many :grades                                                                     
                                                                                                                                                                             
   validates :designation_name, presence: true, uniqueness: { case_sensitive: false }
   
