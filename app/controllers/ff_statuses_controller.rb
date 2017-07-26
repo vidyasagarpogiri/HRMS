2c287353e159f79d6cd00c2fc112b99a41696138
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-07-26 19:37:21 +0530

    layout "leave_template"
    layout "leave_template"

--- app/controllers/ff_statuses_controller.rb
+++ app/controllers/ff_statuses_controller.rb
@@ -1,7 +1,7 @@
 class FfStatusesController < ApplicationController
 	
   before_filter :hr_view,  only: ["new", "edit"]
-  before_filter :other_emp_view
+  before_filter :other_emp_view            
 
 layout "emp_profile_template", only: [:index, :new, :create, :show, :edit, :update
 
