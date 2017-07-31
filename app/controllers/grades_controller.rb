3aa9da60833e2302ff58628b4f19dcf96b9e5806
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-07-31 13:39:31 +0530

    layout "leave_template"
    layout "leave_template"

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,7 +1,7 @@
 class GradesController < ApplicationController 
 
   before_filter :hr_view,  only: ["new", "edit"]
-  before_filter :other_emp_view
+  before_filter :other_emp_view                   
 	
   def index
     @grades = Grade.all            
