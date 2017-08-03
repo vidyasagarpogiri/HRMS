06ca1a6a22c2b359092053bcf8277d03ceda5f4e
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-08-03 16:06:48 +0530

    mount_uploader :avatar, AvatarUploader
    mount_uploader :avatar, AvatarUploader

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -32,7 +32,7 @@ class Employee < ActiveRecord::Base
   has_many :educations
     
   validates :employee_id, presence: true           
-  validates :first_name, presence: true
+  validates :first_name, presence: true         
   validates :last_name, presence: true
   validates :date_of_birth, presence: true            
   validates :department_id, presence: true                                
