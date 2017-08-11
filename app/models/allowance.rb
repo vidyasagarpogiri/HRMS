f822a27f248f136a64814358a00062144bdc2ae1
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-08-11 18:46:23 +0530

    Added Validations
    Added Validations

--- app/models/allowance.rb
+++ app/models/allowance.rb
@@ -2,5 +2,5 @@ class Allowance < ActiveRecord::Base
   
   has_many :salaries_allowances                                                                                                                                          
   has_many :salaries, :through => :salaries_allowances
-  
+             
 end 
