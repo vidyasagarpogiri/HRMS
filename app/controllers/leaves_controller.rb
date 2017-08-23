03201622946cbd5e2c4e7d6f461bd70ca84f3b59
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-08-23 13:34:51 +0530

    layout "leave_template"
    layout "leave_template"

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -25,7 +25,7 @@ class LeavesController < ApplicationController
  def params_leaves
     params.require(:leave).permit(:pl_carry_forward_prev_year, :pl_applied, :sl_applied, :lop_applied)
   end
-    
+              
 end                                                        
                
            
