8574cf649ff6543df50eb30e0afda54bb3cbf2c5
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-08-23 13:38:26 +0530

    layout "leave_template"
    layout "leave_template"

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -1,5 +1,5 @@
 class LeavesController < ApplicationController
- 
+                   
  before_filter :hr_view,  only: ["new", "edit"]
  before_filter :other_emp_view
                                      
