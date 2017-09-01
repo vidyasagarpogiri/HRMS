5b284693f91b7c5925bb5ab19fde94c8849eb476
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-09-01 10:06:51 +0530

    Updated emp_profile_template
    Updated emp_profile_template

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,7 +1,7 @@
 class Experience < ActiveRecord::Base
                   
 belongs_to :employee                                                 
-                                                                                                                                 
+                                                                                                                                         
 validates :previous_company, presence: true
 validates :last_designation, presence: true
 validates :from_date, presence: true
