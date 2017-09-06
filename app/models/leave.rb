25a26ecf200ccb77bc30a97842e1467fffb86164
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-09-06 13:04:02 +0530

    Validations added to model
    Validations added to model

--- app/models/leave.rb
+++ app/models/leave.rb
@@ -1,7 +1,7 @@
 class Leave < ActiveRecord::Base   
                          
   belongs_to :employee                                                                       
-                                                              
+                                                                       
   validates :pl_carry_forward_prev_year, presence: true 
   validates :pl_applied, presence: true
   validates :sl_applied, presence: true
