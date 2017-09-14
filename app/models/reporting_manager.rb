fd7bbbfe23afcd8070a508299db4ed0c2bae5fbf
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-09-14 14:52:30 +0530

    Validations added to model
    Validations added to model

--- app/models/reporting_manager.rb
+++ app/models/reporting_manager.rb
@@ -3,7 +3,7 @@ class ReportingManager < ActiveRecord::Base
   belongs_to :employee
   belongs_to :department
   belongs_to :group
-
+               
   def full_name               
      "#{self.employee.full_name}"
   end
