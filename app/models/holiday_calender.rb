7dc6bb301047f1410cf375784c0d274a6099e117
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-10-03 09:14:44 +0530

    association
    association

--- app/models/holiday_calender.rb
+++ app/models/holiday_calender.rb
@@ -4,7 +4,7 @@ class HolidayCalender < ActiveRecord::Base
   belongs_to :department
   belongs_to :group              
   belongs_to :event    
-                               
+                                         
   #validation
   validates :event_id, :uniqueness => {:scope => :department_id} 
   
