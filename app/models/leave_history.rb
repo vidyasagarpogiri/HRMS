86a261dee292f78b44f6edba743f3c8d99597c89
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-10-18 10:23:29 +0530

    before_create :status_hold
    before_create :status_hold

--- app/models/leave_history.rb
+++ app/models/leave_history.rb
@@ -3,7 +3,7 @@ class LeaveHistory < ActiveRecord::Base
   belongs_to :employee
   belongs_to :leave_type
   before_create :status_hold
-	
+	                
   HOLD = "HOLD"             
   APPROVED = "APPROVED"
   REJECTED = "REJECTED"
