2d0aee534552d342070d6b1d804ce0b6ce85ba63
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-10-20 12:45:10 +0530

    before_action
    before_action

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,7 +3,7 @@ class ProfileController < ApplicationController
   layout "emp_profile_template"             
                                                                                  
   before_filter :hr_view, :only => [ :edit]
-                                                      
+                                                              
   def edit                                                                                                                
     if params[:id].present?                                    
       @employee = Employee.find(params[:id])
