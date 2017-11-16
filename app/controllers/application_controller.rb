174840017ae8d56c78c40be1ebcd9a9e292820e0
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-11-16 14:03:47 +0530

    before_filter :empId
    before_filter :empId

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :empId
   
-                                         
+                                                        
   private
   
   def empId
