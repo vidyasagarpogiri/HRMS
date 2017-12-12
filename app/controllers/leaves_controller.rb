33d4e06a313829d19e6c84246ab9ce0da9d5ae93
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-12-12 13:43:17 +0530

    leave_history
    leave_history

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -4,7 +4,7 @@ class LeavesController < ApplicationController
  before_filter :other_emp_view
                                      
  layout "leave_template"                                                                                                  
-                
+                                 
                       
  def index            
   @leaves = Leave.all
