e57ea9f9007d8005df609e2471795cf03a01f9b0
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2017-12-29 19:14:31 +0530

    before_filter :other_emp_view
    before_filter :other_emp_view

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -21,7 +21,7 @@ class GradesController < ApplicationController
     @grade = Grade.find(params[:id])
     @employees = @grade.employees
   end
-  
+                
   def update
     @grade = Grade.find(params[:id])
     @grade.update(grade_params)
