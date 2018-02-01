5c1d2769addb69858a4ae226405ac8c08e944204
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-02-01 13:04:22 +0530

    before_filter
    before_filter

--- app/controllers/insentives_controller.rb
+++ app/controllers/insentives_controller.rb
@@ -3,7 +3,7 @@ class InsentivesController < ApplicationController
   before_filter :hr_view,  only: ["new", "edit"]                                
                                                             
   before_filter :other_emp_view                         
-                                            
+                                                         
  def index                                                                                                              
   #raise params.inspect                                          
   @salary = Salary.find(params[:salary_id])             
