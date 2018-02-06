010cc45921f35ebc07b942ffeef47b1a607d1a2f
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-02-06 14:54:48 +0530

    before_filter :other_emp_view
    before_filter :other_emp_view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
 
   layout "leave_template"                                                                                           
-  
+   
   before_filter :hr_view,  only: ["new", "edit"]
   before_filter :other_emp_view              
                                   
@@ -32,42 +32,4 @@ class GroupsController < ApplicationController
                          
   def edit            
      @group = Group.find(params[:id])         
-     @reporting_manager = ReportingManager.find_by(@group.id)
-     @employee = Employee.find_by(@reporting_manager.id)
-  end                
-                                                                                                                                       
-  def update                               
-    @group = Group.find(params[:id])                                                                                                     
-    @group.update(group_params)
-    @employee = Employee.find(params[:emp_id])
-    @reporting_manager = ReportingManager.find_by(@group.id)
-    @reporting_manager.update(:employee_id => @employee.id)
-    redirect_to  @group
-  end             
-  
-  def add_employee
-    @group = Group.find(params[:id])
-    @employees = Employee.all    
-  end
-  
-  def update_add_employee
-     @group = Group.find(params[:id])
-     @employee = Employee.find(params[:employee_id])      
-     @employee.update(:group_id => @group.id)
-     redirect_to @group
-  end
-
-  def holiday_list         
-   @holidays = current_user.employee.group.holiday_calenders
-   @leave_policy = current_user.employee.group.leave_policy
-   @group = current_user.employee.group
-  end
-  
-  
-  private
-  def group_params
-     params.require(:group).permit(:group_name)     
-  end
-  
-  
-end            
+     @reporting_manager = ReportingManager.find_by
