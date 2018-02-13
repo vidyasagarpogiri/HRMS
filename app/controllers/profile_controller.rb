13c4c0169d79ece8dcb54ab7ec45a6f14b5c746a
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-02-13 12:57:31 +0530

    before_filter :other_emp_view
    before_filter :other_emp_view

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -12,5 +12,5 @@ class ProfileController < ApplicationController
     end               
     @address = Address.new
   end
-                
+            
 end                                                                     
