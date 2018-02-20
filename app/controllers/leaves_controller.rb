6418216d1199eaf51948796800e148081b84d9ff
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-02-20 13:57:33 +0530

    params_leaves
    params_leaves

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -20,7 +20,7 @@ class LeavesController < ApplicationController
    @employee = Employee.find(params[:employee_id])
    @leave = Leave.create(params_leaves)
    redirect_to employee_leaves_path
- end
+ end                          
                                   
  def params_leaves
     params.require(:leave).permit(:pl_carry_forward_prev_year, :pl_applied, :sl_applied, :lop_applied)
