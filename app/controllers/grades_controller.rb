3dc2f707e31212f94c636f34f180e4a3ac43bb0c
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-03-02 16:09:36 +0530

    before_filter :other_emp_view
    before_filter :other_emp_view

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -2,7 +2,7 @@ class GradesController < ApplicationController
 
   before_filter :hr_view,  only: ["new", "edit"]
   before_filter :other_emp_view                                           
-	                     
+	                                   
   def index                                                              
     @grades = Grade.all                                          
   end                                                                                        
