9d4cfd65853940abe22256488cf70811911ae729
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-04-05 13:00:45 +0530

    layout "emp_profile_template"
    layout "emp_profile_template"

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -15,3 +15,4 @@ class ProfileController < ApplicationController
                                                        
 end                                                                                     
       
+                       
