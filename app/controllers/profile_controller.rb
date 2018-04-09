fed9e8c0ebaaa81640a5d16b49cf028ef6ef4966
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-04-09 14:01:31 +0530

    layout "emp_profile_template"
    layout "emp_profile_template"

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -12,7 +12,6 @@ class ProfileController < ApplicationController
     end               
     @address = Address.new
   end                 
-                                                       
-end                                                                                     
+                                                  
+end                                                                                 
  
-              
