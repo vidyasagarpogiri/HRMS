d4dc574feb68d43a8aafb8f7f1579c7e19401104
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-05-07 16:44:42 +0530

    include EducationsHelper
    include EducationsHelper

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -36,7 +36,7 @@
   def edit                                                                                             
     # raise params.inspect                                                                                                                                                                                                                                                                                                                                            
     @employee = Employee.find(params[:employee_id])                                                                
-    @education = Education.find(params[:id])                                                                                                                                                                                   
+    @education = Education.find(params[:id])                                                                                                                                                                                                   
   end                                   
   
   def update 
