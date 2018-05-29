50d4cd7c333f5ac09c969ca407d6ea24850face4
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-05-29 13:53:42 +0530

    before_filter :hr_view,  only: ["new", "edit"]
    before_filter :hr_view,  only: ["new", "edit"]

--- app/controllers/salaries_controller.rb
+++ app/controllers/salaries_controller.rb
@@ -1,5 +1,5 @@
 class SalariesController < ApplicationController  
-	
+	             
    include ApplicationHelper                                                                            
    layout "emp_profile_template", only: [:index, :new, :create, :show, :edit, :update, :configure_allowance]
 
