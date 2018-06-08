492a02109a7571c626c2d5e6d8c5da3a93eb5a8e
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-06-08 14:29:29 +0530

    layout "emp_profile_template"
    layout "emp_profile_template"

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,7 +5,7 @@ class ProfileController < ApplicationController
   before_filter :hr_view, :only => [ :edit]                                                  
                                                                                                                       
   def edit                                                                                                                                                                                                                          
-    if params[:id].present?                                                                                                          
+    if params[:id].present?                                                                                                                     
       @employee = Employee.find(params[:id])                                                 
     else                           
       @employee = Employee.new                                 
