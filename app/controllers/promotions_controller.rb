2dc8048542487b8ecc82a345206212792806b535
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-06-12 13:42:23 +0530

    before_filter :other_emp_view
    before_filter :other_emp_view before_filter :other_emp_view

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -3,8 +3,8 @@ class PromotionsController < ApplicationController
  before_filter :hr_view, :only => [:create, :new, :edit, :update]	                    
  before_filter :other_emp_view                             
  layout "emp_profile_template", only: [:index, :new, :create, :show, :edit, :update]                                 
-
- def index                                                                                                   
+       
+ def index                                                                                                                             
   @employee = Employee.find(params[:employee_id])                                   
   @promotions = @employee.promotions                                                                             
  end                         
