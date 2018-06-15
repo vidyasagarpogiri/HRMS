9e58d8ed2671710e89095f5750df43d7eda7f687
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-06-15 14:17:51 +0530

    Modified omniauth
    Modified omniauth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -33,3 +33,4 @@ class OmniauthCallbacksController < ApplicationController
                 
                                                                                            
 end                 
+             
