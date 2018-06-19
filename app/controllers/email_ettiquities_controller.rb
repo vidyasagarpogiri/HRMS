89dd42414aeff0a69b3e1271a12a58ee9a24f701
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-06-19 13:05:58 +0530

    before_filter :other_emp_view
     before_filter :other_emp_view

--- app/controllers/email_ettiquities_controller.rb
+++ app/controllers/email_ettiquities_controller.rb
@@ -3,7 +3,7 @@ class EmailEttiquitiesController < ApplicationController
    layout "emp_profile_template", only: [:index, :new, :create, :show]
 
   before_filter :hr_view,  only: ["new", "edit"]
-  before_filter :other_emp_view                              
+  before_filter :other_emp_view                                           
 	               
 def index                        
 # raise params.inspect
