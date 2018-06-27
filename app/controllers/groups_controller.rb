d70d4896283c391414b5d04288dcc518193f777b
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-06-27 14:28:50 +0530

    before_filter :other_emp_view
    before_filter :other_emp_view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
     
   layout "leave_template"                                                                                                                                                                 
-                                             
+                                                                  
   before_filter :hr_view,  only: ["new", "edit"]
   before_filter :other_emp_view              
                                                                        
