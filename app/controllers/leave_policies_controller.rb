f405e417c03144b6ccbdf27d242f2040b4eeb44c
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-08-16 09:43:31 +0530

    layout "leave_template"
    layout "leave_template"

--- app/controllers/leave_policies_controller.rb
+++ app/controllers/leave_policies_controller.rb
@@ -2,7 +2,7 @@ class LeavePoliciesController < ApplicationController
 	
   before_filter :hr_view,  only: ["new", "edit"]                                           
   before_filter :other_emp_view                            
-  layout "leave_template"                                                                                                                       
+  layout "leave_template"                                                                                                                             
                                                                                                                                                                                                              
  def index                                                 
    @department = Department.find(params[:department_id])                                                               
