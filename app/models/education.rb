0806cdc62791194b7accaf6bc5c3ed0d603fe393
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-08-23 12:40:34 +0530

    belongs_to :employee
    belongs_to :employee

--- app/models/education.rb
+++ app/models/education.rb
@@ -4,10 +4,11 @@ class Education < ActiveRecord::Base
   belongs_to :city	
   has_many :qualifications, through: :education_qualifications
   has_many :education_qualifications
-  
+       
   validates :specilization, presence: true                        
   validates :institute, presence: true
   validates :year_of_pass, presence: true
   validates :cgpa_percentage, presence: true	
 end
  
+    
