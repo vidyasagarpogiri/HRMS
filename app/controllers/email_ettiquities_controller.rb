fdb0847bb4a8bc5fa202d416254cbd7f27389efc
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-08-23 13:25:39 +0530

    def destroy
    def destroy

--- app/controllers/email_ettiquities_controller.rb
+++ app/controllers/email_ettiquities_controller.rb
@@ -29,7 +29,7 @@ end
   def show
     @email= EmailEttiquitie.find(params[:id])
   end
-  
+      
   def edit
   end                        
                                                                                                              
