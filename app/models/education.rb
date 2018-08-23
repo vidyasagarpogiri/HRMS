fd113fbaf75511dfe2b3de3ac0d7ea1cfb413a7f
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-08-23 12:40:48 +0530

    belongs_to :employee
    belongs_to :employee

--- app/models/education.rb
+++ app/models/education.rb
@@ -3,7 +3,7 @@ class Education < ActiveRecord::Base
   belongs_to :employee           
   belongs_to :city	
   has_many :qualifications, through: :education_qualifications
-  has_many :education_qualifications
+  has_many :education_qualifications    
        
   validates :specilization, presence: true                        
   validates :institute, presence: true
