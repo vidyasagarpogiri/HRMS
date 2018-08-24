cafa1a4c629f27de4b451a47b56d45683f299fdc
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-08-24 12:51:11 +0530

    belongs_to :address
    belongs_to :address

--- app/models/job_location.rb
+++ app/models/job_location.rb
@@ -2,6 +2,6 @@ class JobLocation < ActiveRecord::Base
                               
   has_many :employees                     
   belongs_to :address    
-                    
+                        
 end                                   
 
