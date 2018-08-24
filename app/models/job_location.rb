0dea6fa3c747b5b956d889f35930bd545edf68e7
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-08-24 12:49:51 +0530

    belongs_to :address
    belongs_to :address

--- app/models/job_location.rb
+++ app/models/job_location.rb
@@ -1,7 +1,7 @@
 class JobLocation < ActiveRecord::Base
                               
   has_many :employees         
-  belongs_to :address
+  belongs_to :address    
                     
 end                         
 
