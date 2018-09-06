c2b61c4c7cececbdc8a6e51052aa62f50a9ef9f8
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-09-06 14:57:02 +0530

    layout "leave_template"
    layout "leave_template"

--- app/controllers/leave_types_controller.rb
+++ app/controllers/leave_types_controller.rb
@@ -2,9 +2,9 @@ class LeaveTypesController < ApplicationController
  
  before_filter :hr_view,  only: ["new", "edit"]                                                                       
  before_filter :other_emp_view                       
- layout "leave_template"                                                        
+ layout "leave_template"                                                           
                                                                                                                          
-  def index                  
+  def index                       
     @leave_types = LeaveType.all                          
   end                                                   
   
