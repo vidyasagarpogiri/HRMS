2891a51fbd4dc7a132b70824f76dbb917773e122
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-10-10 17:10:47 +0530

    include ApplicationHelper
    include ApplicationHelper

--- app/controllers/salaries_controller.rb
+++ app/controllers/salaries_controller.rb
@@ -47,7 +47,7 @@ class SalariesController < ApplicationController
     @ctc_fixed = @salary.gross_salary.to_f + @salary.bonus.to_f+ @salary.gratuity.to_f + @salary.medical_insurance.to_f
     @salary.update(:ctc_fixed => @ctc_fixed, :basic_salary => @salary.basic)
     redirect_to employee_salaries_path(@employee)
-  end
+  end                                   
 
   def show
     @salary =  Salary.find(params[:id])   
