a7e2bfdb1b983f95e76b107044a257322fd7b2ae
Vidya Sagar Pogiri - CES <vidyasagarpogiri@users.noreply.github.com>
2018-10-17 12:15:18 +0530

    layout "emp_profile_template"
    layout "emp_profile_template"

--- app/controllers/email_ettiquities_controller.rb
+++ app/controllers/email_ettiquities_controller.rb
@@ -5,7 +5,7 @@ class EmailEttiquitiesController < ApplicationController
   before_filter :hr_view,  only: ["new", "edit"]
   before_filter :other_emp_view                                               
 	                   
-def index                                              
+def index                                                 
 # raise params.inspect    
  @employee = Employee.find(params[:employee_id])   
  @emails = @employee.email_ettiquities    
