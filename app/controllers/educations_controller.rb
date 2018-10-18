b7083d3927e241b740e6cfe880bcdeb16228349e
Vidya Sagar Pogiri - CES <vidyasagarpogiri@users.noreply.github.com>
2018-10-18 10:25:39 +0530

    EducationsHelper
    EducationsHelper

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -4,7 +4,7 @@
  include EducationsHelper                                                                                                                                                                                                                                 
                                                                                                                                                                   
    layout "emp_profile_template", only: [:index, :new, :create, :show, :edit, :update]       
-   before_filter :hr_view,  only: ["new", "edit"]               
+   before_filter :hr_view,  only: ["new", "edit"]                             
    before_filter :other_emp_view           
                     
   def index         
