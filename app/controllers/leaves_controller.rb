710dfa29fee32a801018c9cc805ed39f1ec6c350
Vidya Sagar Pogiri - CES <vidyasagarpogiri@users.noreply.github.com>
2018-10-25 14:16:22 +0530

    layout "leave_template"
    layout "leave_template"

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -26,5 +26,3 @@ class LeavesController < ApplicationController
   end
                                                                                     
 end 
-               
-                 
