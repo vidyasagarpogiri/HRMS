acf4fbf75b9fedf902ea11f651f199f52be64019
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-11-02 14:48:14 +0530

    layout "leave_template"
    layout "leave_template"

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -36,7 +36,7 @@ class EventsController < ApplicationController
   def update             
    #raise params.inspect
    @event = Event.find(params[:id])
-   @event.update(event_params)                                               
+   @event.update(event_params)                                                              
                                         
    redirect_to events_path
   end
