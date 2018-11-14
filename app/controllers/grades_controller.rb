68c515c35507a19e886952c701aabe59dda37304
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-11-14 13:54:59 +0530

    before_filter :other_emp_view
    before_filter :other_emp_view

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -4,7 +4,7 @@ class GradesController < ApplicationController
   before_filter :other_emp_view                                           
 	                                                 
   def index                                                                            
-    @grades = Grade.all                                                             
+    @grades = Grade.all                                                                                                
   end                                                                                                                
                                                                     
   def new                                            
