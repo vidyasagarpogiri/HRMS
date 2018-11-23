e9056952a0fc78a1a045b78565f3bbc054aa70d9
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-11-23 16:17:18 +0530

    before_filter :other_emp_view
    before_filter :other_emp_view

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -3,7 +3,7 @@ class ExperiencesController < ApplicationController
   layout "emp_profile_template", only: [:index, :new, :create, :show, :edit, :update]
   before_filter :hr_view,  only: ["new", "edit"]
   before_filter :other_emp_view                   
-                             
+                                 
   def index
      @employee = Employee.find(params[:employee_id])            
      @experinces = @employee.experiences              
