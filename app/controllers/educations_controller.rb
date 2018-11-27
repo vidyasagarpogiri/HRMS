d5889fc4f37f8feee613215641acfd0cf49ef279
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-11-27 13:26:05 +0530

    include EducationsHelper
    include EducationsHelper

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -8,7 +8,7 @@
    before_filter :other_emp_view                                    
                     
   def index         
-    @employee = Employee.find(params[:employee_id])
+    @employee = Employee.find(params[:employee_id])     
     @educations =  Education.where(:Employee_id => params[:employee_id])
   end   
   
