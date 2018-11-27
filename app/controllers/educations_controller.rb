af4f238444ac2ccc6973a25df1a14004351191f0
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-11-27 13:55:07 +0530

    before_filter :other_emp_view
    before_filter :other_emp_view

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,11 +1,11 @@
  class EducationsController < ApplicationController               
                                              
  # helper class included                                                                                                                    
- include EducationsHelper                                                                                                                                                                                                                                                    
+ include EducationsHelper                                                                                                                                                                                                                                                                       
                                                                                                                                                                   
    layout "emp_profile_template", only: [:index, :new, :create, :show, :edit, :update]                    
    before_filter :hr_view,  only: ["new", "edit"]                                              
-   before_filter :other_emp_view                                    
+   before_filter :other_emp_view                                        
                     
   def index         
     @employee = Employee.find(params[:employee_id])     
