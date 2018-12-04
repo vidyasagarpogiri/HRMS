35d36edbe8409d5d69b5318bb54dbfc0539c29bd
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-12-04 14:59:40 +0530

    layout "leave_template"
    layout "leave_template"

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,7 @@ class GroupsController < ApplicationController
                                                                                                                                             
   def index              
     @groups = Group.all                                               
-  end                                                                           
+  end                                                                                               
                                                                                                      
   def new 
     @group = Group.new                  
