18edb009905aba16ef9cfa793f2679ce889dbe21
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2018-12-31 14:40:16 +0530

    layout "leave_template"
    layout "leave_template"

--- app/controllers/leave_policies_controller.rb
+++ app/controllers/leave_policies_controller.rb
@@ -2,12 +2,12 @@ class LeavePoliciesController < ApplicationController
 	
   before_filter :hr_view,  only: ["new", "edit"]                                                
   before_filter :other_emp_view                                          
-  layout "leave_template"                                                                                                                                                                                                                                                     
+  layout "leave_template"                                                                                                                                                                                                                                                                         
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
  def index                                                                                                                                                                                                                                                                               
    @department = Department.find(params[:department_id])                                                                                                                                                                                                                                                                                                   
    @leave_policy = @department.leave_policy                                                                                                                                                                                              
- end                                                                                                                                
+ end                                                                                                                                                                  
                                                                                                                                                                     
  def new                                                                                                                                               
   @department = Department.find(params[:department_id])                                                                                                       
