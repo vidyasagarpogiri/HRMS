aa7810ff985709febceabdf195f62622f421ecfd
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-01-08 15:43:18 +0530

    before_filter :other_emp_view
    before_filter :other_emp_view

--- app/controllers/ff_statuses_controller.rb
+++ app/controllers/ff_statuses_controller.rb
@@ -1,6 +1,6 @@
 class FfStatusesController < ApplicationController                                          
 	
-  before_filter :hr_view,  only: ["new", "edit"]                               
+  before_filter :hr_view,  only: ["new", "edit"]                                                                 
   before_filter :other_emp_view                                                                                                                              
                                                             
 layout "emp_profile_template", only: [:index, :new, :create, :show, :edit, :update
