eb76a39c91bf0a01a5696f3cf8e37adea1bd20dc
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-01-25 13:50:12 +0530

    Merged the PR for layout "leave_template"
    Merged the PR for layout "leave_template"

--- app/controllers/leave_histories_controller.rb
+++ app/controllers/leave_histories_controller.rb
@@ -3,7 +3,7 @@ class LeaveHistoriesController < ApplicationController
   #before_filter :hr_view,  only: ["new", "edit"]                                                                   
   #before_filter :other_emp_view                                                                                                                                                                 
 	                                                                                                                                           
-  layout "leave_template"                                                                                                                                                                                                             
+  layout "leave_template"                                                                                                                                                                                                                 
                                                                                                                                               
  include ApplicationHelper                                                                                                                                                                                                                                                                                                                                                
                                                                                                                                                                            
