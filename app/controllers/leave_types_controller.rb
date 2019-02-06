638d2b11f9ada97815dde0e1f4660bc0b4369f33
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-02-06 20:50:00 +0530

    layout "leave_template"
    layout "leave_template"

--- app/controllers/leave_types_controller.rb
+++ app/controllers/leave_types_controller.rb
@@ -6,7 +6,7 @@
                                                                                                                                                                   
   def index                                                                                                                             
     @leave_types = LeaveType.all                                                                                                                                                                                                                                                                                                                       
-  end                                                                                                                                                       
+  end                                                                                                                                                                     
                                                                                                  
   def new                                                                                                                                  
     @leave_type = LeaveType.new                                                                                                                                            
@@ -19,7 +19,7 @@
     redirect_to leave_types_path
   end    
  
-  def edit                                                        
+  def edit                                                          
    # raise params.inspect                                 
     @leave_type = LeaveType.find(params[:id])
   end            
