7f51a8b38ec175686cfc989f47e06baf5c4c2805
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-02-12 12:23:04 +0530

    before_filter :other_emp_view
    before_filter :other_emp_view

--- app/controllers/ff_statuses_controller.rb
+++ app/controllers/ff_statuses_controller.rb
@@ -3,7 +3,7 @@ class FfStatusesController < ApplicationController
   before_filter :hr_view,  only: ["new", "edit"]                                                                               
   before_filter :other_emp_view                                                                                                                                 
                                                                                  
-layout "emp_profile_template", only: [:index, :new, :create, :show, :edit, :update
+layout "emp_profile_template", only: [:index, :new, :create, :show, :edit, :update    
                                                                                                         
   def index                                                                                                   
      @employee = Employee.find(params[:employee_id])                                                                  
