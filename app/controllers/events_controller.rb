d81900a29cd9a5817401279a5f3ddd8208597399
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-02-28 09:05:10 +0530

    before_filter :other_emp_view
    before_filter :other_emp_view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,7 +9,7 @@ class EventsController < ApplicationController
   end
                               
   def show                            
-    @event = Event.find(params[:id])
+    @event = Event.find(params[:id])             
   end
 
  
