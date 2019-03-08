0a6ece47276a57732a7586c53ea899670cbc55a6
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-03-08 09:16:18 +0530

    before_filter :other_emp_view
    before_filter :other_emp_view

--- app/controllers/insentives_controller.rb
+++ app/controllers/insentives_controller.rb
@@ -2,9 +2,9 @@ class InsentivesController < ApplicationController
 	
   before_filter :hr_view,  only: ["new", "edit"]                                
                                                                           
-  before_filter :other_emp_view                                                                                                     
+  before_filter :other_emp_view                                                                                                                       
                                                                                                                                                                                                                                                           
- def index                                                                                                                                                                                                                                                                                                                                                      
+ def index                                                                                                                                                                                                                                                                                                                                                                        
   #raise params.inspect                                                                                   
   @salary = Salary.find(params[:salary_id])                                                                                                                                                                                                                  
   @insentive = @salary.insentives                                                                                                                                         
