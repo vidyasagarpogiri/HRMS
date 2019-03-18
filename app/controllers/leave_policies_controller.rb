aa5935a97eeff0bde33ee9f98e76629d267fe5e3
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-03-18 12:38:55 +0530

    redirect_to leaves
    redirect_to leaves

--- app/controllers/leave_policies_controller.rb
+++ app/controllers/leave_policies_controller.rb
@@ -16,7 +16,7 @@
  
  def create
    @department = Department.find(params[:department_id])
-   @leave_policy = LeavePolicy.create(params_leavepolicy)  
+   @leave_policy = LeavePolicy.create(params_leavepolicy)      
    @leave_policy.department_id = params[:department_id]
    @leave_policy.save
    redirect_to leaves_department_path(@department)
@@ -33,7 +33,7 @@
   @leave_policy = @department.leave_policy                                                                                                                         
   @leave_policy.update(params_leavepolicy)                                                                                                                                                                             
   redirect_to leaves_department_path(@department)                                                                                                                                                                                                        
- end                                                                                                                         
+ end                                                                                                                                                
                                                                                                                                                                                                                                                                                                                                                                                                    
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
  def params_leavepolicy                                                                                                                                                                                                                                                       
