e61f77675e174c907a2086bbe9a45ed385229fd7
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-04-03 09:07:53 +0530

    layout "leave_template"
    layout "leave_template"

--- app/controllers/leave_histories_controller.rb
+++ app/controllers/leave_histories_controller.rb
@@ -1,7 +1,7 @@
 class LeaveHistoriesController < ApplicationController
 	                                                  
   #before_filter :hr_view,  only: ["new", "edit"]                                                                   
-  #before_filter :other_emp_view                                                                                                                                                                 
+  #before_filter :other_emp_view                                                                                                                                                                                     
 	                                                                                                                                                                    
   layout "leave_template"                                                                                                                                                                                                                                                                                                 
                                                                                                                                                                                       
