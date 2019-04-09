f2055db5a361a4dda8b8150ea7bedaf3ae7aef7b
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-04-09 18:47:52 +0530

    layout "leave_template"
    layout "leave_template"

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -7,7 +7,7 @@
                                                                                                                                                                                                                                                                                                                                                                                
  def index                                                                                                                                                                                                                                                                                                                                                                        
   @leaves = Leave.all                                                                                                                                                                                                                             
-  @employee = Employee.find(params[:employee_id])                                                                                                                                                                     
+  @employee = Employee.find(params[:employee_id])                                                                                                                                                                                     
  end                                                                                                                 
  
  def new
