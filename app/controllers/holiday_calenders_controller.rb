bbfb035633b7b2e29c4ff7ad467d76e62cc54c16
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-04-16 12:11:19 +0530

    leave_template
    leave_template

--- app/controllers/holiday_calenders_controller.rb
+++ app/controllers/holiday_calenders_controller.rb
@@ -5,8 +5,8 @@
                                                                                                                                                                       
 def index                                                                                                                     
    @department = Group.find(params[:department_id])                                                                                                                                                                                                         
-   @holiday_calenders =@department.holiday_calenders                                                                                                                                                                                                         
- end                                                                                                                                                                                   
+   @holiday_calenders =@department.holiday_calenders                                                                                                                                                                                                                  
+ end                                                                                                                                                                                                        
                                                                                                                                                                                                                   
   def new                                                                                        
     @holiday_calender = HolidayCalender.new                                                      
