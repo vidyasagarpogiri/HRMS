bde346b4cc9c642f970f9c0ef2287bf0aa1184c3
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-04-16 10:56:31 +0530

    layout "leave_template"
    layout "leave_template"

--- app/controllers/holiday_calenders_controller.rb
+++ app/controllers/holiday_calenders_controller.rb
@@ -3,7 +3,7 @@
   before_filter :other_emp_view                                  
   layout "leave_template"                                                                                                 
                                                                                                                                                                       
-def index                                                                                                      
+def index                                                                                                                     
    @department = Group.find(params[:department_id])                                                                                                                                                                                                         
    @holiday_calenders =@department.holiday_calenders                                                                                                                                                                                                         
  end                                                                                                                                                      
