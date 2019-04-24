f892c149e4f092eae2029492e4a1190843fd7a63
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-04-24 15:47:04 +0530

    roles
    roles

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -5,7 +5,7 @@ class RolesController < ApplicationController
 	                                                                                                                            
   def index                                                                                                                                                                                        
     @roles = Role.all                                                                                                                                                                                                                                                                                  
-  end                                                                                                                                                                                                             
+  end                                                                                                                                                                                                                                  
                                                                                
   def new                                                                                                         
     @role = Role.new                            
