8e6e0bd64efe1b5a98fafe23f5dd2185c5e8a4cd
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-09-17 15:49:42 +0530

    before_filter
    before_filter

--- app/controllers/leave_policies_controller.rb
+++ app/controllers/leave_policies_controller.rb
@@ -1,5 +1,5 @@
          class LeavePoliciesController < ApplicationController                            
-	                         
+	                                                    
   before_filter :hr_view,  only: ["new", "edit"]                                                
   before_filter :other_emp_view                                                                                                                                                         
   layout "leave_template"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
