57b99846964193b5b24e818d7a46a646d99f74ab
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2020-01-08 10:56:58 +0530

    leave_template
    leave_template

--- app/controllers/leave_histories_controller.rb
+++ app/controllers/leave_histories_controller.rb
@@ -1,4 +1,4 @@
-class LeaveHistoriesController < ApplicationController
+class LeaveHistoriesController < ApplicationController      
 	                                                  
   #before_filter :hr_view,  only: ["new", "edit"]                                                                   
   #before_filter :other_emp_view                                                                                                                                                                                                      
