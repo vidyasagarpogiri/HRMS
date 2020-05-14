303210329e25f41fd61028308820e66f0ae057a7
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2020-05-14 13:44:32 +0530

    before_filter
    before_filter

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -12,7 +12,7 @@ class EmployeesController < ApplicationController
                                                                                                                                                                                                                                                                                                                                                                        
   def new                                                                                                                                                                                        
     @employee = Employee.new                                                                                                                                                            
-                                                                                                                                                                 
+                                                                                                                                                                              
   def create                                                                                                           
      @employee = Employee.create(params_employees)   
     if @employee.errors.present?                               
