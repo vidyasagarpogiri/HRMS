a731c8acf708edc85f77ff8e030d0a1c71b8b572
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2020-06-23 22:01:17 +0530

    before_filter
    before_filter

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -34,8 +34,8 @@
      @employee = Employee.find(params[:id])
      if @employee.educations.present?
      @specialization = @employee.educations.order('year_of_pass DESC').first
-     end
-  end        
+     end                                    
+  end              
   
   def edit
     @employee = Employee.find(params[:id])
