ce7dca28b2ca480ba40741329cf5905fd24b4b2d
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2020-07-18 15:32:03 +0530

    exception
    exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     resource, id = request.path.split('/')[1,2]                   
     @id= id
     if resource == "employees" && id.present?
-      @emp = Employee.find(id)                               
+      @emp = Employee.find(id)                                              
     end
                                
   end
