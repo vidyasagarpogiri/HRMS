8400e5bd39c3085e35af5d94a76ca46f79ad2226
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2020-08-04 17:42:04 +0530

    ReportingManager
    ReportingManager

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -23,7 +23,7 @@
                                                          
   def show                                                                                     
     @employee = Employee.find(params[:id])                                                                       
-    if @employee.reporting_managers.first.present? &&  @employee.reporting_managers.first.manager_id.present? 
+    if @employee.reporting_managers.first.present? &&  @employee.reporting_managers.first.manager_id.present?              
       unless @employee.reporting_managers.first.manager_id == 0 
         @reporting_manager = Employee.find(@employee.reporting_managers.first.manager_id).full_name                
       end                                                               
