8720a9ad3ffd4e520c5138064d58701d97e9738a
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2020-11-04 11:40:43 +0530

    holiday_calenders
    holiday_calenders

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -29,7 +29,7 @@ class GroupsController < ApplicationController
      @holiday_calenders = @group.holiday_calenders                      
   end               
        
-  def edit                                    
+  def edit                                        
      @group = Group.find(params[:id])                                                                                                               
      @reporting_manager = ReportingManager.find_by                             
   end                                                                   
