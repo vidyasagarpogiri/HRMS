76253778630e04e535f59776ef0d5bc9ddcbf0a8
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2020-11-10 10:55:12 +0530

    leave_template
    leave_template

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-                                     
+                                                      
   layout "leave_template"                                                                                                                                                                              
                                                                                           
   before_filter :other_emp_view                                                                                    
