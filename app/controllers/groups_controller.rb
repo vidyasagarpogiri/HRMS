dd14a5164362c289a511fbb0453f77674434473e
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2020-11-12 07:34:13 +0530

    leave_template
    leave_template

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,7 +24,7 @@ class GroupsController < ApplicationController
                                                 
   def show                                       
      @group = Group.find(params[:id])
-     @employee = @group.reporting_manager.employee    
+     @employee = @group.reporting_manager.employee                             
      @leave_policy = @group.leave_policy
      @holiday_calenders = @group.holiday_calenders                      
   end                                           
