b68e16f34386d7cfb2385e040e989bc16d32e74f
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2020-11-19 08:56:57 +0530

    leave_template
    leave_template

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,7 @@ class GroupsController < ApplicationController
   def index                                                                                       
     @groups = Group.all                                                                                                                                                     
   end                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                                                          
+                                                                                                                                                                                                                                                                                                         
   def new                                                                                                                                                                                                                                                                                              
     @group = Group.new                                                                                                                   
   end                                                          
