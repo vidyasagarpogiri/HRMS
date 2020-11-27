0e2dafa2d23c350e81f15428e1e284eca8c5ccee
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2020-11-27 07:46:23 +0530

    GroupsController
    GroupsController

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,7 +10,7 @@ class GroupsController < ApplicationController
                                                                                                                                                                                                                                                                                                                                             
   def new                                                                                                                                                                                                                                                                                                                                                               
     @group = Group.new                                                                                                                                            
-  end                                                          
+  end                                                                     GroupsController
                                             
   def edit                                                 
      @group = Group.find(params[:id])                                                                                                                                             
