fcbd9b13b2970ca6f0f4ecfba342499a88d50686
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2020-11-27 07:45:34 +0530

    GroupsController
    GroupsController

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,7 @@ class GroupsController < ApplicationController
                                                       
   layout "leave_template"                                                                                                                                                                                                 
                                                                                           
-  before_filter :other_emp_view                                                                                    
+  before_filter :other_emp_view                                                                                                  
                                                                                                                                                                                                                                                                                           
   def index                                                                                                                                                      
     @groups = Group.all                                                                                                                                                                            
