c46f03d29e2dc5b1853209661a00eb9bac2fc148
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2021-01-13 09:11:56 +0530

    hr_view
    hr_view

--- app/controllers/insentives_controller.rb
+++ app/controllers/insentives_controller.rb
@@ -27,7 +27,7 @@ class InsentivesController < ApplicationController
  # redirect_to employee_salary_path(@employee, @salary)
  end                             
                                        
- def show                                                               
+ def show                                                                   
   # raise params.inspect                             
  end                                                                                                                     
                                                                                                                                                                                                                                                           
