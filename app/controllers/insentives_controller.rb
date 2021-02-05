93ba1acc4fcd3992e195e54fe4d55392b5b1c86b
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2021-02-05 09:36:46 +0530

    other_emp_view
    other_emp_view

--- app/controllers/insentives_controller.rb
+++ app/controllers/insentives_controller.rb
@@ -29,7 +29,7 @@ class InsentivesController < ApplicationController
  end                             
                                                         
  def show                                                                   
-  # raise params.inspect                                    
+  # raise params.inspect                                        
  end                                                                                                                     
                                                                                                                                                                                                                                                           
  def edit                                                                                                                                                                                                                                  
