67adb1b4e88539aa3328d06f92c0c8baaf1b2117
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2021-02-12 09:46:52 +0530

    ApplicationController
    ApplicationController

--- app/controllers/insentives_controller.rb
+++ app/controllers/insentives_controller.rb
@@ -13,7 +13,7 @@ class InsentivesController < ApplicationController
  def new                                                                                                                                                                                                                                                                                                       
    @employee= Employee.find(params[:employee_id])                                                                                                                                                                      
    @salary = Salary.find(params[:salary_id])                                                                                        
- end                                                    
+ end                                                                  
                                          
  def create                              
  #raise params.inspect
