9354d46a573e4d4282459266b4ef6d3e314ebe6d
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2021-03-01 10:03:22 +0530

    before_filter
    before_filter

--- app/controllers/insentives_controller.rb
+++ app/controllers/insentives_controller.rb
@@ -11,47 +11,7 @@ class InsentivesController < ApplicationController
  end                                                          
 	                                                                                                                                                                                                                                                                                                                                                          
  def new                                                                                                                                                                                                                                                                                                                                                                                                                                                      
-   @employee= Employee.find(params[:employee_id])                                                                                                                                                                                                                                                                                                                                   
-   @salary = Salary.find(params[:salary_id])                                                                                                                                            
- end                                                                                                          
-                                            
- def create                              
- #raise params.inspect
-  @form_type = params[:commit]
-  @insentive1 = Insentive.create(params_insentive)                                        
-  @insentive1.save
-  @salary = Salary.find(params[:salary_id])
-  @insentives =  @salary.insentives
-  @employee= Employee.find(params[:employee_id])
-  @insentive = Insentive.new
- # redirect_to employee_salary_path(@employee, @salary)                     
- end                             
-                                                             
- def show                                                                         
-  # raise params.inspect                                           
- end                                                                                                                                 
-                                                                                                                                                                                                                                                                 
- def edit                                                                                                                                                                                                                                  
-  @employee= Employee.find(params[:employee_id])                                                                                                                                   
-  @salary = Salary.find(params[:salary_id])                                                                                                              
-  @insentive = Insentive.find(params[:id])                                                                                                                                                                                                                                                                                                                                                                                                                                  
- end                                                                                  
- 
- def update
-  @employee= Employee.find(params[:employee_id])
-  @salary = Salary.find(params[:salary_id])
-  @insentive = Insentive.find(params[:id])
-  @insentive.update(params.require(:insentive).permit(:insentive_type, :value))
-  redirect_to employee_salaries_path(@employee)
- end
-
- def destroy
- @employee= Employee.find(params[:employee_id])
-  @salary = Salary.find(params[:salary_id])
-  @insentive = Insentive.find(params[:id])
-	@insentive.destroy    
-	redirect_to employee_salaries_path(@employee)
-	end
+   @employee= Employee.find(params[:employee_id])                                                                                                    
  
  private
               
