919493c637740d28cc4d5baf7b0775e4140d60ac
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2021-03-10 11:36:28 +0530

    hr_view
    hr_view

--- app/controllers/insentives_controller.rb
+++ app/controllers/insentives_controller.rb
@@ -12,7 +12,7 @@ class InsentivesController < ApplicationController
 	                                                                                                                                                                                                                                                                                                                                                                                                     
  def new                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
    @employee= Employee.find(params[:employee_id])                                                                                                                                                                                                                      
-                                                                          hr_view
+                                                                        
  private
                                                           
   def params_insentive                             
