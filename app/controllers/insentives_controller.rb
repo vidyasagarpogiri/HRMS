f64fd345c11ac6eeed7586ee9bb63f8a40a70566
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2021-04-26 13:43:52 +0530

    hr_view
    hr_view

--- app/controllers/insentives_controller.rb
+++ app/controllers/insentives_controller.rb
@@ -12,7 +12,7 @@ class InsentivesController < ApplicationController
  def new                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
                                                                                                                                                                                                                                                                                     
  private                                                                           
-                                                                            
+                                                                                            
   def params_insentive                                       
     params.require(:insentive).permit(:insentive_type, :value).merge(:salary_id => params[:salary_id])
   end
