0935fd10111e7d2b9a517faf3b89d91c744a3c62
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2021-05-17 08:07:05 +0530

    other_emp_view
    other_emp_view

--- app/controllers/insentives_controller.rb
+++ app/controllers/insentives_controller.rb
@@ -1,10 +1,11 @@
   class InsentivesController < ApplicationController              
 	   
   before_filter :hr_view,  only: ["new", "edit"]                                                 
-                                                                                             
+                                                                                               
   before_filter :other_emp_view                                                                                                                                                  
                                                                                                                                                                                                                                                                                                                               
  def index                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
   #raise params.inspect                                                                                                                                                                                                                                                                                                                           
   @salary = Salary.find(params[:salary_id])                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
  end
+   
