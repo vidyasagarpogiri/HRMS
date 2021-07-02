db4a23fe18df773981785b103118118c6b1783dd
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2021-07-02 14:46:37 +0530

    other_emp_view
    other_emp_view

--- app/controllers/insentives_controller.rb
+++ app/controllers/insentives_controller.rb
@@ -8,5 +8,5 @@
   #raise params.inspect                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
   @salary = Salary.find(params[:salary_id])                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
  end                     
-       
+          
        
