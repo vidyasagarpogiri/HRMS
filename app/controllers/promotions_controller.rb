5acf3af537b9db7fe9a85b6876abb45a713b6e8b
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2021-10-07 21:56:10 +0530

    emp_profile_template
    emp_profile_template

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -28,7 +28,7 @@ else
 end                                                       
   
   def edit   
-    @employee = Employee.find(params[:employee_id])      
+    @employee = Employee.find(params[:employee_id])        
     @promotion = Promotion.find(params[:id])   
   end                  
                   
