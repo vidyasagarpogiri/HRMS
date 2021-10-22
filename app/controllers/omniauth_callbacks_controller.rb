d7fb84635c968ca8d8a17d0149310f6ca01f7a5f
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2021-10-22 16:17:35 +0530

    other_emp_view
    other_emp_view

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -9,7 +9,7 @@ class OmniauthCallbacksController < ApplicationController
       @user1.token= @user.token         
       @user1.secret=  @user.secret
       @user1.save!
-      @user = @user1
+      @user = @user1  
     end              
                                    
     if request.env["omniauth.auth"]["extra"]["raw_info"]["hd"] == "amzur.com"  
