68ea9c8b1c2397bc7b90830f30075a0284097eb4
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2021-10-26 16:35:43 +0530

    other_emp_view
    other_emp_view

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -8,7 +8,7 @@
   @employee = Employee.find(params[:employee_id])                                                                                                                                                                                        
   @promotions = @employee.promotions                                                                                                                                                             
  end                                                                   
-	                                                                                            
+	                                                                                                        
   def new                                                         
     @promotion = Promotion.new                              
     @employee = Employee.find(params[:employee_id])                                 
