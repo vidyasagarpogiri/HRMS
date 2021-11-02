38822b2e217f7175001d4c76f28fc0f103c179f4
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2021-11-02 17:17:07 +0530

    leave_template
    leave_template

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -25,7 +25,7 @@
     params.require(:leave).permit(:pl_carry_forward_prev_year, :pl_applied, :sl_applied, :lop_applied)
   end
                                                                             
-end 
+end   
                                
                      
                        
