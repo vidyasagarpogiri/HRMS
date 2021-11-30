3314e977fa733dbc479e599fcae5b306ca296c8e
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2021-11-30 20:52:25 +0530

    other_emp_view
    other_emp_view

--- app/controllers/insentives_controller.rb
+++ app/controllers/insentives_controller.rb
@@ -7,6 +7,6 @@
  def index                                                                                                               
                              
                               
-                                                                         
+                                                                            
        
   
