609b92fcefb30bdc88e11166fc552a80c0867820
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2021-12-15 11:25:13 +0530

    other_emp_view
    other_emp_view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
                                                       
   layout "leave_template"                                                                                                                                                                                                 
-                                                                                          
+                                                                                                            
   before_filter :other_emp_view                                                                                                                   
                                                                                                                                                                                                                                                                                                         
   def index                                                                                                                                                                                                                                           
