9f88e7bffb158f1769da73a8d446407f46bacc9f
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2022-07-29 19:21:48 +0530

    other_emp_view
    other_emp_view

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -6,6 +6,6 @@ validates :previous_company, presence: true
 validates :last_designation, presence: true
 validates :from_date, presence: true
 validates :to_date, presence: true
-      
+             
 end       
             
