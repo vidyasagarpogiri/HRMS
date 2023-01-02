91ea0f784f89e9ffc455c166d8805f3edaee9b7d
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2023-01-02 21:54:20 +0530

    other_emp_view
    other_emp_view

--- app/controllers/insentives_controller.rb
+++ app/controllers/insentives_controller.rb
@@ -2,7 +2,7 @@ class InsentivesController < ApplicationController
 	                
   before_filter :hr_view,  only: ["new", "edit"]                            
 	                                                              
-  before_filter :other_emp_view                    
+  before_filter :other_emp_view                      
   end                                                                        
   other_emp_view    
  
