6d8f750092617a399522567491afd2bb14425daf
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2023-07-19 00:15:36 +0530

    Update events_controller.rb
    other_emp_view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  
+    
   layout "leave_template"        
   before_filter :hr_view,  only: ["new", "edit"]
   before_filter :other_emp_view               
