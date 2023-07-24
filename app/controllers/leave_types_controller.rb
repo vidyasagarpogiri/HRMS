2300741f471e4b8a821c3a2fb8264543801f147c
vidyasagarpogiri <vidyasagarpogiri@gmail.com>
2023-07-24 10:18:05 +0530

    Changed the controller in email

--- app/controllers/leave_types_controller.rb
+++ app/controllers/leave_types_controller.rb
@@ -1,4 +1,4 @@
-    class LeaveTypesController<Controller                                                                   
+    class LeaveTypesController<Controller                                                                                            
  
  before_filter :hr_view,  only: ["new", "edit"]                                                                                             
  before_filter :other_emp_view                                                                                                                                    
