cff8ae64d108522de9d35c1f0de1094bacca4d0c
vidyasagarpogiri <vidyasagarpogiri@gmail.com>
2024-01-07 11:18:05 +0530

    Changed the controller in email

--- app/controllers/ff_statuses_controller.rb
+++ app/controllers/ff_statuses_controller.rb
@@ -1,4 +1,4 @@
-class FfStatusesController < ApplicationController                                          
+class FfStatusesController < Controller                                            
 	
   before_filter :hr_view,  only: ["new", "edit"]                                                                               
   before_filter :other_emp_view                                                                                                                                 
