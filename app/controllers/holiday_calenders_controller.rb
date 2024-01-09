a04775c0b1ee384bbd48072be38c9c9e76455a5a
vidyasagarpogiri <vidyasagarpogiri@gmail.com>
2024-01-09 11:18:05 +0530

    Changed the controller in email

--- app/controllers/holiday_calenders_controller.rb
+++ app/controllers/holiday_calenders_controller.rb
@@ -1,4 +1,4 @@
-  class HolidayCalendersController < ApplicationController
+  class HolidayCalendersController < Controller  
   before_filter :hr_view,  only: ["new", "edit"]     
   before_filter :other_emp_view                                  
   layout "leave_template"                                                                                                           
