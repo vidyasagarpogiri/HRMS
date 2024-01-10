035738439075d447ba9fcf59ee7b5d2a5876386c
vidyasagarpogiri <vidyasagarpogiri@gmail.com>
2024-01-10 11:18:05 +0530

    Changed the controller in email

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,4 +1,4 @@
- class EducationsController < ApplicationController               
+ class EducationsController < Controller                 
                                              
  # helper class included                                                                                                                    
  include EducationsHelper                                                                                                                                                                                                                                                                       
