70bb92440cbe87d0dc9f0e0cf5d163a459d9c1fd
vidyasagarpogiri <vidyasagarpogiri@gmail.com>
2024-01-18 11:18:05 +0530

    Changed the controller in email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class Controller   < ActionController::Base
+class Controller        < ActionController     ::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   
