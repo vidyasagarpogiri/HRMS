0e6e41d9f0c3c1041c3bc2c36f07bd971cf449ee
vidyasagarpogiri <vidyasagarpogiri@gmail.com>
2024-01-29 11:18:05 +0530

    Changed the controller in email

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -1,4 +1,4 @@
- class LeavesController < Controller        
+ class LeavesController      < Controller             
                    
  before_filter :hr_view,  only: ["new", "edit"]
  before_filter :other_emp_view                         
