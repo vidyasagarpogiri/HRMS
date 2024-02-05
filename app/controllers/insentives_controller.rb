e392ea87dab60eca0b62b8dbb9be3babc8db92ae
vidyasagarpogiri <vidyasagarpogiri@gmail.com>
2024-02-05 11:18:05 +0530

    Changed the controller in email

--- app/controllers/insentives_controller.rb
+++ app/controllers/insentives_controller.rb
@@ -1,4 +1,4 @@
-   class InsentivesController      < Controller        
+   class InsentivesController< Controller        
   before_filter :hr_view,  only: ["new", "edit"]                                                                      
   before_filter :other_emp_view                                                                                                       
  end 
