4a6d36b43b0cc2dbd4f48253e11b66715d818f5b
vidyasagarpogiri <vidyasagarpogiri@gmail.com>
2024-03-06 11:18:05 +0530

    Changed the controller in email

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,4 +1,4 @@
-  class RolesController<Controller                                    
+  class RolesController<Controller                                                  
 	                                                                                                     
   before_filter :hr_view,  only: ["new", "edit"]                                                                                 
   before_filter :other_emp_view                                                                                                                                                                                                                                                         
